7dc93f312a5e5af7cfa7a8dac680858f3ccf80a7
april-dev <66917271+april-dev@users.noreply.github.com>
2020-06-22 18:19:37 -0400

    Update 733. Flood Fill.java

--- Graph/733. Flood Fill/733. Flood Fill.java
+++ Graph/733. Flood Fill/733. Flood Fill.java
@@ -23,4 +23,19 @@
         }
         return image;
     }
-    
+   
+//DFS
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if (image[sr][sc] == newColor) return image;
+        int oldColor = image[sr][sc];
+        dfs(image, sr, sc, newColor, oldColor);
+        return image;
+    }
+    public void dfs(int[][] image, int i, int j, int newColor, int oldColor){
+        if (i<0 || i>=image.length || j<0 || j>=image[0].length || image[i][j]!=oldColor) return;
+        image[i][j] = newColor;
+        dfs(image, i+1, j, newColor, oldColor);
+        dfs(image, i-1, j, newColor, oldColor);
+        dfs(image, i, j-1, newColor, oldColor);
+        dfs(image, i, j+1, newColor, oldColor);
+    }
