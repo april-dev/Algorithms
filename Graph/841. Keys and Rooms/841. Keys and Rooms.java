f627282bdaf81caff5f859685c852beca71844b5
april-dev <66917271+april-dev@users.noreply.github.com>
2020-06-23 18:17:07 -0400

    Create 841. Keys and Rooms.java

---
+++ Graph/841. Keys and Rooms/841. Keys and Rooms.java
@@ -0,0 +1,34 @@
+public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        Queue<List<Integer>> queue = new LinkedList<>();
+        HashSet<Integer> visited = new HashSet<>();
+        queue.offer(rooms.get(0));
+        visited.add(0);
+        
+        while (!queue.isEmpty()){
+            List<Integer> cur = queue.remove();
+            for (int key:cur){
+                if (visited.contains(key)) continue;
+                queue.offer(rooms.get(key));
+                visited.add(key);
+                if (visited.size()==rooms.size()) return true;
+            }
+        }
+        return visited.size()==rooms.size();
+    }
+    
+    
+    
+      public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        Stack<Integer> dfs = new Stack<>(); dfs.add(0);
+        HashSet<Integer> seen = new HashSet<Integer>(); seen.add(0);
+        while (!dfs.isEmpty()) {
+            int i = dfs.pop();
+            for (int j : rooms.get(i))
+                if (!seen.contains(j)) {
+                    dfs.add(j);
+                    seen.add(j);
+                    if (rooms.size() == seen.size()) return true;
+                }
+        }
+        return rooms.size() == seen.size();
+    }
