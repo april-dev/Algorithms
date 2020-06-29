ee52addabd757c14f10c49a3cf27ed2c038b872b
april-dev <66917271+april-dev@users.noreply.github.com>
2020-06-29 10:05:11 -0400

    Update 1499. Max Value of Equation.java

--- Weekly Contest/1499. Max Value of Equation/1499. Max Value of Equation.java
+++ Weekly Contest/1499. Max Value of Equation/1499. Max Value of Equation.java
@@ -1,3 +1,7 @@
+
+//Priority queue
+//O(nlogn)
+
 class Solution {
     public int findMaxValueOfEquation(int[][] points, int k) {
         int res = Integer.MIN_VALUE;
@@ -13,3 +17,19 @@ class Solution {
         return res;
     }
 }
+
+//mono deque
+//O(n)
+
+       Deque<Integer> dq = new ArrayDeque<>();
+        int n = ps.length, res = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            while (!dq.isEmpty() && ps[i][0] - ps[dq.peekFirst()][0] > k) dq.pollFirst();
+            if (!dq.isEmpty()) {
+                int j = dq.peekFirst();
+                res = Math.max(res, ps[i][1] + ps[j][1] + ps[i][0] - ps[j][0]);
+            }
+            while (!dq.isEmpty() && ps[i][1] - ps[i][0] >= ps[dq.peekLast()][1] - ps[dq.peekLast()][0]) dq.pollLast();
+            dq.offerLast(i);
+        }
+        return res;
