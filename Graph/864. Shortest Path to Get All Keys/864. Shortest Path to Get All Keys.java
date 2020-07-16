6b5237a02da011b9c4ec4e177517965ba73d68e5
april-dev <66917271+april-dev@users.noreply.github.com>
2020-07-16 17:41:30 -0400

    Update 864. Shortest Path to Get All Keys.java

--- Graph/864. Shortest Path to Get All Keys/864. Shortest Path to Get All Keys.java
+++ Graph/864. Shortest Path to Get All Keys/864. Shortest Path to Get All Keys.java
@@ -62,3 +62,58 @@ class Solution {
         return -1;
     }
 }
+
+
+
+//This solution is added to better understand the state relationship
+/**
+The idea is based on the thoughts that, usually, you won't want to visit a position where you have traversed.
+However, in this problem, you may have to go back and forth to get keys for the lock.
+So the keypoint is "how to relax the visited condition"?
+The keypoint is that "you are only allowed to revisit a position if you get new keys".
+To solve it, I use a visited map considering current carried keys, and prevent to revisit the same position if hold key is not updated.
+Original visited map is two-dimension, and with keys dimension added, it extends to a three-dimension vector.
+Since the problem tells us that 1<=K<=6, we know at most 64 conditions here. (only have to consider key, and no need to consider lock status)
+*/
+int shortestPathAllKeys(vector<string>& grid) {
+    int m=grid.size(), n=m?grid[0].size():0;
+    if(!m || !n) return 0;
+    int path=0, K=0;
+    vector<int> dirs={0,-1,0,1,0};
+    vector<vector<vector<bool>>> visited(m,vector<vector<bool>>(n,vector<bool>(64,0))); //at most 6 keys, using bitmap 111111
+    queue<pair<int,int>> q; //<postion, hold keys mapping>
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(grid[i][j]=='@'){
+                q.push({i*n+j,0});
+                visited[i][j][0]=1;                    
+            }
+            if(grid[i][j]>='A' && grid[i][j]<='F') K++; //total alpha number
+        }
+    }
+    while(!q.empty()){
+        int size=q.size();
+        for(int i=0;i<size;i++){
+            int a=q.front().first/n, b=q.front().first%n;
+            int carry=q.front().second;
+            q.pop();        
+            if(carry==((1<<K)-1)) return path; //if all keys hold, just return 
+            for(int j=0;j<4;j++){
+                int x=a+dirs[j], y=b+dirs[j+1], k=carry;
+                if(x<0 || x>=m || y<0 || y>=n || grid[x][y]=='#') continue;
+                if(grid[x][y]>='a' && grid[x][y]<='f'){
+                    k=carry|(1<<(grid[x][y]-'a')); //update hold keys
+                }
+                else if(grid[x][y]>='A' && grid[x][y]<='F'){
+                    if(!(carry & (1<<(grid[x][y]-'A')))) continue;
+                }
+                if(!visited[x][y][k]){
+                    visited[x][y][k]=1;
+                    q.push({x*n+y,k});
+               }                
+            }
+        }
+        path++;
+    }
+    return -1;
+}
