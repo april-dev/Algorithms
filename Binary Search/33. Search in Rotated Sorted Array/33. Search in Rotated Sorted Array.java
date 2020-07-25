94a65627d28ac5d3da5fc6aa25b63c60c2bb66e5
april-dev <66917271+april-dev@users.noreply.github.com>
2020-07-24 23:07:20 -0400

    Create 33. Search in Rotated Sorted Array.java

---
+++ Binary Search/33. Search in Rotated Sorted Array/33. Search in Rotated Sorted Array.java
@@ -0,0 +1,32 @@
+public int search(int[] nums, int target) {
+       if (nums == null || nums.length == 0) return -1;
+        
+       int left = 0, right = nums.length-1;
+        int start = 0;
+        //1. find index of the smallest element
+        while(left < right) {
+            int mid = left + (right-left)/2;
+            if (nums[mid] > nums[right]) {
+                left = mid +1;
+            } else right = mid;
+        }
+        
+     //2. figure out in which side our target lies
+        start = left;
+        left = 0;
+        right = nums.length-1;
+        if (target >= nums[start] && target <= nums[right])
+            left = start;
+        else right = start;
+        
+     //3. Run normal binary search in sorted half.
+        while(left <= right) {
+            int mid = left + (right - left)/2;
+            if (nums[mid] == target) return mid;
+            
+            if (nums[mid] > target) right = mid-1;
+            else left = mid + 1;
+        }
+        
+        return -1;
+    }
