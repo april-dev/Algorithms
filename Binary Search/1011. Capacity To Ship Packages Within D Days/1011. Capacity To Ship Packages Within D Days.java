95ea7b86fb2aec045e61239df5b1ae8bd8285af7
april-dev <66917271+april-dev@users.noreply.github.com>
2020-07-29 12:22:16 -0400

    Create 1011. Capacity To Ship Packages Within D Days.java

---
+++ Binary Search/1011. Capacity To Ship Packages Within D Days/1011. Capacity To Ship Packages Within D Days.java
@@ -0,0 +1,39 @@
+public int shipWithinDays(int[] weights, int D) {
+        int left = 0, right = 0;
+        for (int weight:weights){
+            left = Math.max(left, weight);
+            right += weight;
+        }
+        
+        while (left<right){
+            int mid = left + (right - left)/2;
+            int count = 1;
+            int sum = 0;
+            
+           /*
+            for (int w:weights){
+                if (sum+w>mid){
+                    count+=1;
+                    sum=0;
+                }
+                sum+=w;
+            }
+            */
+            for (int w:weights){
+                if (sum+w>mid){
+                    count+=1;
+                    sum=w;
+                }else{
+                    sum+=w;
+                }
+            
+            }
+            
+            if (count>D){
+                left = mid+1;
+            }else{
+                right = mid;
+            }
+        }
+        return right;
+    }
