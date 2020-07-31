6795200367b253ca490ae39d5c7c731b3109c8f9
april-dev <66917271+april-dev@users.noreply.github.com>
2020-07-31 16:00:52 -0400

    Update 378. Kth Smallest Element in a Sorted Matrix.java

--- Binary Search/378. Kth Smallest Element in a Sorted Matrix/378. Kth Smallest Element in a Sorted Matrix.java
+++ Binary Search/378. Kth Smallest Element in a Sorted Matrix/378. Kth Smallest Element in a Sorted Matrix.java
@@ -16,9 +16,9 @@ public int kthSmallest(int[][] matrix, int k) {
         int left = matrix[0][0], right = matrix[n-1][n-1]+1;
         while (left<right){
             int mid = left + (right - left)/2;
-            int count = 0;
+            int count = 0, j = n-1;
             for (int i=0; i<n; i++){
-                int j = n-1;
+           
                 while (j>=0 && matrix[i][j]>mid ) j--;
                 count += (j+1);
             }
