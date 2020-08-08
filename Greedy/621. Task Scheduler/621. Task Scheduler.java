ef967af9c6777e698ddc4a263ff7e4d07fcf53a4
april-dev <66917271+april-dev@users.noreply.github.com>
2020-08-08 14:17:47 -0400

    Update 621. Task Scheduler.java

--- Greedy/621. Task Scheduler/621. Task Scheduler.java
+++ Greedy/621. Task Scheduler/621. Task Scheduler.java
@@ -1,3 +1,7 @@
+//special case: more than one task with most frequency
+//A B ? ? A B ? ? A B
+//think of sequence "A B" as a special task "X", then we got:
+//X ? ? X ? ? X
 public int leastInterval(char[] tasks, int n) {
         int[] counter = new int[26];
         int max = 0;
