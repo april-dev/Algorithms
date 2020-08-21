f3641d8e8972ede5184a034d892b87d4e611abe9
april-dev <66917271+april-dev@users.noreply.github.com>
2020-08-20 22:58:08 -0400

    Update 153. Find Minimum in Rotated Sorted Array.java

--- Binary Search/153. Find Minimum in Rotated Sorted Array/153. Find Minimum in Rotated Sorted Array.java
+++ Binary Search/153. Find Minimum in Rotated Sorted Array/153. Find Minimum in Rotated Sorted Array.java
@@ -5,6 +5,8 @@ public int findMin(int[] nums) {
             if (nums[mid]>nums[right]){
                 left = mid+1;
             }else{
+            //this else condition includes nums[mid]<=nums[right], although nums[mid]==nums[right] will never happen (no duplicates), right = mid not mid-1 because in the case of
+            //nums[mid]<nums[right], and nums[mid] is the minimum value, setting right = mid-1 will miss this value (for example [ 3,1,2])
                 right = mid;
             }
         }
