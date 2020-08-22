7dea164612225f5296800142a9b299e8e36cdcf0
april-dev <66917271+april-dev@users.noreply.github.com>
2020-08-22 15:04:50 -0400

    Update 912. Sort an Array.java

--- DIvide and Conquer/912. Sort an Array/912. Sort an Array.java
+++ DIvide and Conquer/912. Sort an Array/912. Sort an Array.java
@@ -51,3 +51,76 @@ public int[] sortArray(int[] nums) {
         }
         return nums;
     }
+
+//Heap sort
+public int[] sortArray(int[] nums) {
+        for (int i=nums.length/2-1; i>=0; i--){
+            heapify(nums, i, nums.length-1);
+        }
+        for (int i=nums.length-1; i>=1; i--){
+            swap(nums, 0, i);
+            heapify(nums, 0, i-1);
+        }
+        return nums;
+    }
+    
+    public void heapify(int[] nums, int i, int end){
+        while (i<=end){
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            int maxIdx = i;
+            if (left<=end && nums[left]>nums[maxIdx]) maxIdx = left;
+            if (right<=end && nums[right]>nums[maxIdx]) maxIdx = right;
+            if (maxIdx==i) break;
+            swap(nums, i, maxIdx);
+            i = maxIdx;
+        }
+    }
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+//Top down merge sort
+public int[] sortArray(int[] nums) {
+        sort(nums, 0 , nums.length-1);
+        return nums;
+    }
+    
+    public void sort(int[] nums, int l, int r){
+        if (l>=r) return;
+        int mid = l+ (r-l)/2;
+        sort(nums, l, mid);
+        sort(nums, mid+1, r);
+        merge(nums, l,mid,r);    
+    }
+    public void merge(int[] nums, int l, int m, int r){
+        int[] temp = new int[r-l+1];
+        int i=l, j=m+1;
+        int k=0;
+        while (i<=m && j<=r){
+            if (nums[i]<nums[j]){
+                temp[k] = nums[i];
+                k++;
+                i++;
+            }else{
+                temp[k] = nums[j];
+                k++;
+                j++;
+            }
+        }
+        while (i<=m){
+            temp[k] = nums[i];
+                k++;
+                i++;
+        }
+        while (j<=r){
+            temp[k] = nums[j];
+                k++;
+                j++;
+        }
+        for (int p=l, q=0; p<=r; p++, q++){
+            nums[p] = temp[q];
+        }
+    }
