70a2fcd02784ab13d3dd6b1ea4ea9cef9880bc21
april-dev <66917271+april-dev@users.noreply.github.com>
2020-08-22 01:36:51 -0400

    Update 912. Sort an Array.java

--- DIvide and Conquer/912. Sort an Array/912. Sort an Array.java
+++ DIvide and Conquer/912. Sort an Array/912. Sort an Array.java
@@ -30,3 +30,24 @@ public int[] sortArray(int[] nums) {
         }
         return nums;
     }
+
+//Selection sort
+public int[] sortArray(int[] nums) {
+        int n = nums.length;
+        for (int i=0; i<n; i++){
+            int min = i;
+            for (int j=i+1; j<n; j++){
+                if (nums[j]<nums[min]){
+                    min = j;
+                }
+                
+            }
+            if (min!=i){
+                //swap(nums, i, min)
+                int temp = nums[i];
+                nums[i] = nums[min];
+                nums[min] = temp;
+            }
+        }
+        return nums;
+    }
