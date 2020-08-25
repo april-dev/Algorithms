0a6582711f147e4e564f79bc60c9151446ee54a4
april-dev <66917271+april-dev@users.noreply.github.com>
2020-08-25 19:43:49 -0400

    Create 42. Trapping Rain Water.java

---
+++ Stack/42. Trapping Rain Water/42. Trapping Rain Water.java
@@ -0,0 +1,23 @@
+public int trap(int[] height) {
+        int i=0, j=height.length-1;
+        int leftmax=0, rightmax=0;
+        int res = 0;
+        while(i<j){
+           leftmax = Math.max(leftmax, height[i]);
+           rightmax = Math.max(rightmax, height[j]);
+           if (leftmax<rightmax){
+               res += (leftmax-height[i]);
+               i++;
+           }else{
+               res += (rightmax-height[j]);
+               j--;
+           }
+        }            
+        return res;
+    }
+    
+    
+    //Stack (find local minimum)
+    
+    
+    
