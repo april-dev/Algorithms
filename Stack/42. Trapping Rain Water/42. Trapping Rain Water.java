c3cde6d2fd82302797c9c883ae383ee42db5cc52
april-dev <66917271+april-dev@users.noreply.github.com>
2020-08-25 19:56:59 -0400

    Update 42. Trapping Rain Water.java

--- Stack/42. Trapping Rain Water/42. Trapping Rain Water.java
+++ Stack/42. Trapping Rain Water/42. Trapping Rain Water.java
@@ -18,6 +18,19 @@ public int trap(int[] height) {
     
     
     //Stack (find local minimum)
-    
+    //add water horizontally, example [3,2,1,0,3]
+    public int trap(int[] height) {
+        Stack<Integer> stack = new Stack<>();
+        int res = 0;
+        for (int i=0; i<height.length; i++){
+            while (!stack.isEmpty() && height[stack.peek()]<height[i]){
+                int idx = stack.pop();
+                int width = stack.isEmpty()? 0: i-stack.peek()-1;
+                res += (Math.min(height[i], stack.isEmpty()? 0:height[stack.peek()])-height[idx])*width;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
     
     
