428ec4af0ee142eaf901571e514625724130491a
april-dev <66917271+april-dev@users.noreply.github.com>
2020-08-30 17:11:37 -0400

    Update 676. Implement Magic Dictionary.java

--- Trie/676. Implement Magic Dictionary/676. Implement Magic Dictionary.java
+++ Trie/676. Implement Magic Dictionary/676. Implement Magic Dictionary.java
@@ -49,3 +49,49 @@ class MagicDictionary {
         public TrieNode(){}
     }
 }
+
+
+//fuzzy search
+/*
+Time Complexity:
+buildDict: O(n*m)
+n: numbers of words
+m: length of word
+search: O(m)
+m: length of word
+Space Complexity:
+O(n*m)
+/*
+class MagicDictionary {
+    Map<String, HashSet<Character>> map = new HashMap<>();
+    /** Initialize your data structure here. */
+    public MagicDictionary() {
+        
+    }
+    
+    public void buildDict(String[] dictionary) {
+        for (String s:dictionary){
+            for (int i=0; i<s.length(); i++){
+                String temp =s.substring(0, i) +"*" + s.substring(i+1);
+                HashSet<Character> set = map.getOrDefault(temp, new HashSet<>());
+                set.add(s.charAt(i));
+                map.put(temp, set);
+            }
+        }
+    }
+    
+    public boolean search(String searchWord) {
+        char[] s = searchWord.toCharArray();
+        for (int i=0; i<searchWord.length(); i++){
+            char c = s[i];
+            s[i] = '*';
+            String t = new String(s);
+            if (map.containsKey(t)){
+                HashSet set = map.get(t);
+                if (!set.contains(c) || set.size()>1) return true;
+            }
+            s[i] = c;
+        }
+        return false;
+    }
+}
