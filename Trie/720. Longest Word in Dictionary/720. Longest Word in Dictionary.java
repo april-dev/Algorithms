44fc6cdff158eee41c58245d85ef7b91a8f21b6c
april-dev <66917271+april-dev@users.noreply.github.com>
2020-08-31 22:39:07 -0400

    Update 720. Longest Word in Dictionary.java

--- Trie/720. Longest Word in Dictionary/720. Longest Word in Dictionary.java
+++ Trie/720. Longest Word in Dictionary/720. Longest Word in Dictionary.java
@@ -34,6 +34,59 @@ class MagicDictionary {
 }
 
 
+//Trie + DFS
+class Solution {
+    class TrieNode{
+        String word = "";
+        TrieNode[] children = new TrieNode[26];
+    }
+
+TrieNode root = new TrieNode();
+public String longestWord(String[] words) {
+    if(words == null || words.length == 0) return "";
+    
+    for(String word: words){
+        TrieNode cur = root;
+        for(char c: word.toCharArray()){
+            if(cur.children[c - 'a'] == null)
+                cur.children[c - 'a'] = new TrieNode();
+            cur = cur.children[c - 'a'];
+        }
+        cur.word = word;
+    }
+    
+    return dfs(root);
+}
+
+public String dfs(TrieNode node){
+    String res = node.word;
+    for(TrieNode child: node.children){
+        if(child != null && child.word.length() != 0){ 
+            String childWord = dfs(child);
+            if(childWord.length() > res.length() || (childWord.length() == res.length() && childWord.compareTo(res) < 0))
+                res = childWord;
+        }
+    }
+    return res;
+}
+    
+//DFS with global variable
+    private void dfs(TreeNode node) {
+		if (node == null)
+			return;
+
+		if (node.word != null) {
+			if (node.word.length() > result.length())
+				result = node.word;
+			else if (node.word.length() == result.length() && node.word.compareTo(result) < 0)
+				result = node.word;
+		}
+
+		for (TreeNode child : node.children)
+			if (child != null && child.word != null)
+				dfs(child);
+	}
+
 //Trie + No sorting
 class Solution {
     TrieNode root = new TrieNode();
