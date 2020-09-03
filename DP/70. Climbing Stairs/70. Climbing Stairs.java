8f24802c1edb1d3a418f75daf5dbbb2ae077007d
april-dev <66917271+april-dev@users.noreply.github.com>
2020-09-03 13:22:28 -0400

    Create 70. Climbing Stairs.java

---
+++ DP/70. Climbing Stairs/70. Climbing Stairs.java
@@ -0,0 +1,11 @@
+public int climbStairs(int n) {
+        int[] ans = new int[n];
+        if (n==1) return 1;
+        if (n==2) return 2;
+        ans[0]=1;
+        ans[1]=2;
+        for (int i=2; i<n; i++){
+            ans[i]=ans[i-1]+ans[i-2];
+        }
+        return ans[n-1];
+    }
