78dbedbcab434c85b74a6c0d017549e2d09e4c43
april-dev <66917271+april-dev@users.noreply.github.com>
2020-09-06 11:17:14 -0400

    Create 198. House Robber.java

---
+++ DP/198. House Robber/198. House Robber.java
@@ -0,0 +1,13 @@
+public int rob(int[] nums) {
+        int n = nums.length;
+        if (n==0) return 0;
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        
+        for (int i=1; i<n; i++){
+            dp[i+1] = Math.max(dp[i-1]+nums[i], dp[i]);
+        }
+        
+        return dp[n];
+    } 
