152a751c7f932bd778ff8d4a9f9d86affa93f503
april-dev <66917271+april-dev@users.noreply.github.com>
2020-09-07 17:55:06 -0400

    Update 139. Word Break.java

--- DP/139. Word Break/139. Word Break.java
+++ DP/139. Word Break/139. Word Break.java
@@ -17,3 +17,35 @@ public boolean wordBreak(String s, List<String> wordDict) {
         }
         return dp[s.length()];
     }
+
+//Brute force O(2^N)
+/*
+Given an array of length n, there are n+1 ways/intervals to partition it into two parts. Each interval has two choices - split or not. 
+In the worse case, we will have to check all possibilities, which becomes O(2^(n+1)) -> O(2^n). 
+*/
+
+/*
+T(N) = T(N-1) + T(N-2) + ... + T(0)
+T(N-1) = T(N-2) + ... + T(0)
+T(N) - T(N-1) = T(N-1)
+T(N) = 2*T(N-1)
+O(2^N)
+*/
+
+ public boolean wordBreak(String s, List<String> wordDict) {
+        // put all words into a hashset
+        Set<String> set = new HashSet<>(wordDict);
+        return wb(s, set);
+    }
+    private boolean wb(String s, Set<String> set) {
+        int len = s.length();
+        if (len == 0) {
+            return true;
+        }
+        for (int i = 1; i <= len; ++i) {
+            if (set.contains(s.substring(0, i)) && wb(s.substring(i), set)) {
+                return true;
+            }
+        }
+        return false;
+    }
