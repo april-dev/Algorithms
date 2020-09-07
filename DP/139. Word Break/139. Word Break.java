288df337a75e70f8f6d69b93a4ddebe29681ca05
april-dev <66917271+april-dev@users.noreply.github.com>
2020-09-07 17:30:02 -0400

    Update 139. Word Break.java

--- DP/139. Word Break/139. Word Break.java
+++ DP/139. Word Break/139. Word Break.java
@@ -1,3 +1,9 @@
+//dp[i] stands for whether subarray(0, i) can be segmented into words from the dictionary. So dp[0] means whether subarray(0, 0) (which is an empty string) can be segmented, 
+//and of course the answer is yes.
+
+//The default value for boolean array is false. Therefore we need to set dp[0] to be true.
+
+
 public boolean wordBreak(String s, List<String> wordDict) {
         boolean[] dp = new boolean[s.length()+1];
         dp[0]=true;
