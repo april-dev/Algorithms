02449024a7322d9688d384d8464c6e6eeb15e1be
april-dev <66917271+april-dev@users.noreply.github.com>
2020-09-10 18:02:23 -0400

    Update 376. Wiggle Subsequence.java

--- DP/376. Wiggle Subsequence/376. Wiggle Subsequence.java
+++ DP/376. Wiggle Subsequence/376. Wiggle Subsequence.java
@@ -20,7 +20,7 @@ public int wiggleMaxLength(int[] nums) {
         return Math.max(up[n-1], down[n-1]);
     }
     
-    //save space
+    //save space (also a greedy solution)
     public int wiggleMaxLength(int[] nums) {
         int n = nums.length;
         if (n==0) return 0;
@@ -52,3 +52,31 @@ public int wiggleMaxLength(int[] nums) {
     return count;
 }
     
+//O(N^2) solution
+//similar to longest increasing subsequence
+//however, For each i, we do not need to compare all the previous conditions and update the max, cuz for both up and down array, 
+//its length keeps increasing(+0 or +1) with either copying from last element or plus 1 operation.
+//And, if we reduce the space to O(1). It would be the exactly same as the most up-voted greedy solution.
+
+//similar to waves, pick up turning point at very local max or min.
+
+public int wiggleMaxLength(int[] nums) {
+        if (nums.length==0) return 0;
+        int[] up = new int[nums.length];
+        int[] down = new int[nums.length];
+        Arrays.fill(up, 1);
+        Arrays.fill(down, 1);
+        int res = 1;
+        for (int i=1; i<nums.length; ++i){
+            for (int j=0; j<i; ++j){
+                if(nums[j]<nums[i]){
+                    up[i] = Math.max(up[i], down[j]+1);
+                }else if (nums[j]>nums[i]){
+                    down[i] = Math.max(down[i], up[j]+1);
+                }
+            }
+            int max = Math.max(up[i], down[i]);
+            res = Math.max(res, max);
+        }
+        return res;
+    }
