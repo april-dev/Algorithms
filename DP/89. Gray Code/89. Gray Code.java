dc66dd50a97afc93a76c6245922c4a869c089e4f
april-dev <66917271+april-dev@users.noreply.github.com>
2020-09-12 01:05:43 -0400

    Create 89. Gray Code.java

---
+++ DP/89. Gray Code/89. Gray Code.java
@@ -0,0 +1,12 @@
+public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList<>();
+        res.add(0);
+        
+        for (int i=0; i<n; i++){
+            int resSize = res.size();
+            for (int k=resSize-1; k>=0; k--){
+                res.add(res.get(k) | 1 << i);
+            }
+        }
+        return res;
+    }
