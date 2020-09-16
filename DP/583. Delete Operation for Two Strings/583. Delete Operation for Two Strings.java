f8d8b4322f8ed7349ad977cabf58f235cb8b77b5
april-dev <66917271+april-dev@users.noreply.github.com>
2020-09-16 02:57:05 -0400

    Create 583. Delete Operation for Two Strings.java

---
+++ DP/583. Delete Operation for Two Strings/583. Delete Operation for Two Strings.java
@@ -0,0 +1,38 @@
+//Same as Q72. Edit Distance
+public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m+1][n+1];
+        
+        for (int i=1; i<=m; i++) dp[i][0] = i;
+        for (int i=1; i<=n; i++) dp[0][i] = i;
+        
+        for (int i=1; i<=m; i++){
+            for (int j=1; j<=n; j++){
+                if (word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.min(dp[i-1][j-1]+2, Math.min(dp[i-1][j]+1, dp[i][j-1]+1));
+                }
+            }
+        }
+        return dp[m][n];
+    }
+    
+  //Same as Q1143. Longest Common Subsequence
+  public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m+1][n+1];
+        
+        for (int i=1; i<=m; i++){
+            for (int j=1; j<=n; j++){
+                if (word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }else{
+                    dp[i][j] =Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return m+n-2*dp[m][n];
+    }
