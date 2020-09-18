75b9ef38ba53b96cc32fed7fe20a8ae31dc22920
april-dev <66917271+april-dev@users.noreply.github.com>
2020-09-17 22:40:24 -0400

    Create 1092. Shortest Common Supersequence.java

---
+++ DP/1092. Shortest Common Supersequence/1092. Shortest Common Supersequence.java
@@ -0,0 +1,43 @@
+//Q1143 Longest common subsequence
+class Solution {
+    public String shortestCommonSupersequence(String str1, String str2) {
+        String lcs = longestSubSeq(str1, str2);
+        int i=0, j=0;
+        StringBuilder sb = new StringBuilder();
+        
+        for (char c:lcs.toCharArray()){
+            while(str1.charAt(i)!=c){
+                sb.append(str1.charAt(i));
+                i++;
+            }
+            while(str2.charAt(j)!=c){
+                sb.append(str2.charAt(j));
+                j++;
+            }
+            sb.append(c);
+            i++;
+            j++;
+        }
+        sb.append(str1.substring(i));
+        sb.append(str2.substring(j));
+        return sb.toString();   
+    }
+    public String longestSubSeq(String str1, String str2){
+        int m = str1.length();
+        int n = str2.length();
+        String[][] dp = new String[m+1][n+1];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], "");
+        }
+        for (int i=1; i<=m; i++){
+            for (int j=1; j<=n; j++){
+                if (str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + str1.charAt(i-1);
+                }else{
+                    dp[i][j] = dp[i-1][j].length() > dp[i][j-1].length()? dp[i-1][j]:dp[i][j-1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
