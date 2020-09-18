6c54f614c94a6f26d735a896951914cd8f0d4145
april-dev <66917271+april-dev@users.noreply.github.com>
2020-09-17 23:19:50 -0400

    Update 1092. Shortest Common Supersequence.java

--- DP/1092. Shortest Common Supersequence/1092. Shortest Common Supersequence.java
+++ DP/1092. Shortest Common Supersequence/1092. Shortest Common Supersequence.java
@@ -41,3 +41,32 @@ class Solution {
         return dp[m][n];
     }
 }
+
+
+//If use string dp, memory limit exceed beccause ut store the super sequence. using string DP passes in Q1143 Longest common subsequence because it stores sub-sequence
+public String shortestCommonSupersequence(String str1, String str2) {
+       int m = str1.length();
+       int n = str2.length();
+       String[][] dp = new String[m + 1][n + 1];
+        
+       for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], "");
+        }
+        
+        for (int i = 0; i <= m; i++){
+            for (int j = 0; j <= n; j++){
+                if (i==0 && j==0){
+                    dp[i][j] = "";
+                }else if (i == 0){
+                    dp[i][j] = dp[i][j-1] + str2.charAt(j-1);
+                }else if (j == 0){
+                    dp[i][j] = dp[i-1][j] + str1.charAt(i-1);;
+                }else if (str1.charAt(i - 1) == str2.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j - 1] + str1.charAt(i - 1);
+                }else{
+                    dp[i][j] = dp[i - 1][j].length()<dp[i][j - 1].length()? dp[i-1][j] + str1.charAt(i-1):dp[i][j-1] + str2.charAt(j-1) ;
+                }
+            }
+        }
+        return dp[m][n];
+    }
