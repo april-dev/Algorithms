20a610c0d7415d4f0918a2a926c81f9d1b97c8de
april-dev <66917271+april-dev@users.noreply.github.com>
2020-09-22 16:10:49 -0400

    Update 688. Knight Probability in Chessboard.java

--- DP/688. Knight Probability in Chessboard/688. Knight Probability in Chessboard.java
+++ DP/688. Knight Probability in Chessboard/688. Knight Probability in Chessboard.java
@@ -18,3 +18,31 @@ class Solution {
         return ans;
     }
 }
+
+public double knightProbability(int N, int K, int r, int c) {
+        int[][] dirs = {{-1, -2},{-2, -1}, {-2, 1}, {-1, 2}, {1, 2},{2, 1},{2, -1},{1, -2}};
+        double[][] dp_prev = new double[N][N];
+        double res = 0;
+        dp_prev[r][c] = 1;
+        for (int k=1; k<=K; k++){
+            double[][] dp = new double[N][N];
+            for (int i=0; i<N; i++){
+                for (int j=0; j<N; j++){
+                    for (int[] dir:dirs){
+                        int newRow = i + dir[0];
+                        int newCol = j + dir[1];
+                        if (newRow<N && newRow>=0 && newCol<N && newCol>=0){
+                            dp[newRow][newCol] += dp_prev[i][j]/8;
+                        }
+                    }
+                }
+            }
+            dp_prev = dp;
+        }
+        for (int i=0; i<N; i++){
+            for (int j=0; j<N; j++){
+                res += dp_prev[i][j];
+            }
+        }
+        return res;
+    }
