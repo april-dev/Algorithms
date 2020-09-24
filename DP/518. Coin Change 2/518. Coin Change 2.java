1648cb5dae00561be2dd9687ac6354fa32770f60
april-dev <66917271+april-dev@users.noreply.github.com>
2020-09-24 17:57:58 -0400

    Update 518. Coin Change 2.java

--- DP/518. Coin Change 2/518. Coin Change 2.java
+++ DP/518. Coin Change 2/518. Coin Change 2.java
@@ -8,3 +8,21 @@
         }
         return dp[amount];
     }
+
+public int change(int amount, int[] coins) {
+        if (amount == 0) return 1;
+        if (coins.length == 0) return 0;
+        Integer[][] dp = new Integer[coins.length+1][amount+1];
+        return helper(amount, coins, dp, 0);
+    }
+    public int helper(int amount, int[] coins, Integer[][] dp, int idx){
+        if (amount<0 || idx == coins.length) return 0;
+        if (amount==0) return 1;
+        if (dp[idx][amount]!=null) return dp[idx][amount];
+        
+        int sum1 = helper(amount, coins, dp, idx+1);
+        int sum2 = helper(amount-coins[idx], coins, dp, idx);
+    
+        dp[idx][amount] = sum1+sum2;
+        return dp[idx][amount];
+    }
