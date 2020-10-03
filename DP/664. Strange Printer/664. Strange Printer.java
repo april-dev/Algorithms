63003764d29b752778afc32553a9fea175371f7e
april-dev <66917271+april-dev@users.noreply.github.com>
2020-10-03 16:17:11 -0400

    Update 664. Strange Printer.java

--- DP/664. Strange Printer/664. Strange Printer.java
+++ DP/664. Strange Printer/664. Strange Printer.java
@@ -22,3 +22,25 @@ public int strangePrinter(String s) {
     
     
 //top down
+class Solution {
+    public int strangePrinter(String s) {
+        int n = s.length();
+        if (n==0) return 0;
+        int[][] dp = new int[n][n];
+        for (int i=0; i<n; i++) dp[i][i] = 1;
+        return helper(s, 0, n-1, dp);
+    }
+    public int helper(String s, int i, int j, int[][] dp){
+        if (i>j) return 0;
+        if (dp[i][j]>0) return dp[i][j];
+        
+        int res = s.length()+1;
+        for (int k=i; k<j; k++){
+            int turn = helper(s, i, k, dp) + helper(s, k+1, j, dp);
+            if (s.charAt(k)==s.charAt(j)) res = Math.min(res, turn-1);
+            else res = Math.min(res, turn);
+        }
+        dp[i][j] = res;
+        return res;
+    }
+}
