482b2a99164a1925317468a422e67d94978f16a3
april-dev <66917271+april-dev@users.noreply.github.com>
2020-10-07 13:47:10 -0400

    Update 1223. Dice Roll Simulation.java

--- DP/1223. Dice Roll Simulation/1223. Dice Roll Simulation.java
+++ DP/1223. Dice Roll Simulation/1223. Dice Roll Simulation.java
@@ -1,3 +1,12 @@
+/*
+dp[i][j] := # of sequences of length i end with j
+dp[i][j] := sum(dp[i-1]) – invalid
+
+Time complexity: O(n*6)
+Space complexity: O(n*6)
+
+*/
+
 public int dieSimulator(int n, int[] rollMax) {
         int mod = (int) 1e9 + 7;
         int[][] dp = new int[n+1][7];
@@ -17,4 +26,44 @@ public int dieSimulator(int n, int[] rollMax) {
             }
         }
         return dp[n][6];
+    }
+
+
+/*
+def: dp[i][j][k] := # of sequences ends with k consecutive j after i rolls
+Init: dp[1][*][1] = 1
+
+transition:
+dp[i][j][1] = sum(dp[i-1][p][*]), p != j
+dp[i][j][k + 1] = dp[i-1][j]][k]
+
+Time complexity: O(n*6*6*15)
+Space complexity: O(n*6*15) -> O(6*15)
+*/
+
+public int dieSimulator(int n, int[] rollMax) {
+        int mod = (int) 1e9 + 7;
+        int[][][] dp = new int[n+1][6][16];
+        
+        // dp[i][j][k] := # of sequences ends with k consecutive j after i rolls
+        for (int i=0; i<6; i++) dp[1][i][1] = 1;
+        
+        for (int i=2; i<=n; i++){
+            for (int j=0; j<6; j++){
+                for (int p=0; p<6; p++){
+                    for (int k=1; k<=rollMax[p]; k++){
+                        if (p!=j) dp[i][j][1] = (dp[i][j][1] + dp[i-1][p][k])%mod;
+                        else if (k<rollMax[p]) dp[i][j][k+1] = (dp[i][j][k+1] + dp[i-1][p][k])%mod;
+                    }
+                }
+            }
+        }
+        int ans = 0;
+        for (int j=0; j<6; j++){
+            for (int k = 1; k <= rollMax[j]; k++){
+                ans = (ans + dp[n][j][k])%mod;
+            }
+        }
+        return ans;
+        
     }
