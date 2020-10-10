7bd4926b8ae46d46b390514af228cbad3c5eeb1d
april-dev <66917271+april-dev@users.noreply.github.com>
2020-10-09 23:07:40 -0400

    Create 297. Serialize and Deserialize Binary Tree.java

---
+++ Binary Tree/297. Serialize and Deserialize Binary Tree/297. Serialize and Deserialize Binary Tree.java
@@ -0,0 +1,36 @@
+public class Codec {
+    private static final String spliter = ",";
+    private static final String NN = "X";
+    
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        buildString(root, sb);
+        return sb.toString();
+    }
+    public void buildString(TreeNode root, StringBuilder sb){
+        if (root==null) sb.append(NN + spliter);
+        else{
+            sb.append(root.val + spliter);
+            buildString(root.left, sb);
+            buildString(root.right, sb);
+        }   
+    }
+    
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] s = data.split(spliter);
+        Queue<String> q = new LinkedList<>();
+        for (String cur:s) q.add(cur);
+        return buildTree(q);
+    }
+    public TreeNode buildTree(Queue<String> q){
+        String val = q.remove();
+        if (val.equals(NN)) return null;
+        TreeNode root = new TreeNode(Integer.valueOf(val));
+        root.left = buildTree(q);
+        root.right = buildTree(q);
+        return root;
+    }
+}
