f2554251fb53297ea357b8efaf9c35e977708c48
april-dev <66917271+april-dev@users.noreply.github.com>
2020-10-14 17:05:49 -0400

    Update 213. House Robber II.java

--- DP/213. House Robber II/213. House Robber II.java
+++ DP/213. House Robber II/213. House Robber II.java
@@ -1,3 +1,12 @@
+/*
+At the first glance, I think the perfect way to split the problem is 1. not rob the 1st house; 2. rob 1st house, 
+because 1 and 2 won't have any intersection (code below follows this idea and beats 100%). 
+However, the way this solution splits this problem is 1. not rob the 1st house; 2. not rob the last house. 
+As you can see, the second statement of these two split strategies are different and they are not logically equal 
+because "not rob the last house" means you can choose to rob the 1st house or not. Then why it is still correct? 
+It is because the 2nd statement from the 2nd strategy contains the 2nd statement from the 1st strategy. 
+In other words, the 2nd set has some overlap with the 1st set in the 2nd strategy. Since our goal is only to find the max, it is okay to include some overlap.
+*/
 public int robhelper(int[] nums, int low, int high){
         int pre_i2=0;
         int pre_i1=0;
