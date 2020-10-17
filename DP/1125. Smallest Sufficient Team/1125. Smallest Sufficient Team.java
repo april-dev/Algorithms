cc3433817e1ce837c5e2ae9ce8faf653631bc59e
april-dev <66917271+april-dev@users.noreply.github.com>
2020-10-17 15:12:11 -0400

    Update 1125. Smallest Sufficient Team.java

--- DP/1125. Smallest Sufficient Team/1125. Smallest Sufficient Team.java
+++ DP/1125. Smallest Sufficient Team/1125. Smallest Sufficient Team.java
@@ -39,3 +39,43 @@ public int[] smallestSufficientTeam(String[] req_skills, List<List<String>> peop
         }
         return res;
     }
+
+//reduce dp from O(N^2) to O(N)
+public int[] smallestSufficientTeam(String[] req_skills, List<List<String>> people) {
+        int n = req_skills.length;
+        int ppl = people.size();
+            
+        Map<String, Integer> map = new HashMap<>();
+        for (int i=0; i<n; i++) map.put(req_skills[i], i);
+        
+        int[] dp = new int[1<<n];
+        int[][] path = new int[1<<n][2];
+
+        Arrays.fill(dp, Integer.MAX_VALUE/2);
+        dp[0] = 0;
+
+        for (int i=1; i<=people.size(); i++){
+            int k = 0;
+            for (String s:people.get(i-1)) k |= (1<<map.get(s));
+            for (int j=(1<<n)-1; j>=0; j--){
+                if ( dp[j] + 1<dp[j | k]) {
+                    dp[j | k] =  dp[j] + 1;
+                    path[j | k][0] = j;
+                    path[j | k][1] = i;
+                }
+            }
+        }
+            
+        int target = (1<<n) - 1;
+        Stack<Integer> stack = new Stack<>();
+        while (target>0){
+            stack.add(path[target][1]-1);
+            target = path[target][0];
+        }
+            
+        int[] res = new int[stack.size()];
+        for (int i=0; i<res.length; i++){
+            res[i] = stack.pop();
+        }
+        return res;
+    }
