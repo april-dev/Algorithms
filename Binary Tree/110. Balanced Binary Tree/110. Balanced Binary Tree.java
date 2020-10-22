167942a70450abb95e99040ad4d2014bf7cc3dfc
april-dev <66917271+april-dev@users.noreply.github.com>
2020-10-22 18:22:52 -0400

    Update 110. Balanced Binary Tree.java

--- Binary Tree/110. Balanced Binary Tree/110. Balanced Binary Tree.java
+++ Binary Tree/110. Balanced Binary Tree/110. Balanced Binary Tree.java
@@ -1,4 +1,4 @@
-//Time: O(nlogn), worst case O(N^2) for spindly tree
+//Multiple pass, Time: O(nlogn), worst case O(N^2) for spindly tree
 class Solution {
     public boolean isBalanced(TreeNode root) {
         if (root==null) return true;
@@ -10,5 +10,41 @@ class Solution {
     public int depth(TreeNode root){
         if (root==null) return 0;
         return 1 + Math.max(depth(root.left), depth(root.right));
+    }
+}
+
+
+//One pass, time: O(n)
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return helper(root) != -1;
+    }
+    public int helper(TreeNode root){
+        if (root==null) return 0;
+        int left = helper(root.left);
+        if (left==-1) return -1;
+        int right = helper(root.right);
+        if (right==-1) return -1;
+        
+        if (Math.abs(left-right)>1) return -1;
+        
+        return Math.max(left, right) + 1;
+    }
+}
+
+//Alternatives, but much slower if the tree is heavily skewed right
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return helper(root) != -1;
+    }
+    public int helper(TreeNode root){
+        if (root==null) return 0;
+        
+        int left = helper(root.left);
+        int right = helper(root.right);
+               
+        if (left==-1 || right==-1 || Math.abs(left-right)>1) return -1;
+        
+        return Math.max(left, right) + 1;
     }
 }
