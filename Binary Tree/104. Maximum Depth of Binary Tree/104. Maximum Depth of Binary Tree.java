0208dc88501964745cb956f51e950c960ce11a4c
april-dev <66917271+april-dev@users.noreply.github.com>
2020-10-22 13:49:47 -0400

    Update 104. Maximum Depth of Binary Tree.java

--- Binary Tree/104. Maximum Depth of Binary Tree/104. Maximum Depth of Binary Tree.java
+++ Binary Tree/104. Maximum Depth of Binary Tree/104. Maximum Depth of Binary Tree.java
@@ -48,3 +48,21 @@ public int maxDepth(TreeNode root) {
         }
         return maxDepth;
     }
+
+//Iterative (BFS)
+public int maxDepth(TreeNode root) {
+        if (root==null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        int depth = 0;
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            for (int i=0; i<size; i++){
+                TreeNode cur = queue.poll();
+                if (cur.left!=null) queue.offer(cur.left);
+                if (cur.right!=null) queue.offer(cur.right);
+            }
+            depth++;
+        }
+        return depth;
+    }
