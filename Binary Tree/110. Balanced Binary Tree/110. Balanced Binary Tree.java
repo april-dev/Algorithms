c77465ce0271faeaf6ef825265fe9cf89eb52005
april-dev <66917271+april-dev@users.noreply.github.com>
2020-10-22 18:02:39 -0400

    Update 110. Balanced Binary Tree.java

--- Binary Tree/110. Balanced Binary Tree/110. Balanced Binary Tree.java
+++ Binary Tree/110. Balanced Binary Tree/110. Balanced Binary Tree.java
@@ -1,3 +1,4 @@
+//Time: O(nlogn), worst case O(N^2) for spindly tree
 class Solution {
     public boolean isBalanced(TreeNode root) {
         if (root==null) return true;
@@ -5,6 +6,7 @@ class Solution {
         int right = depth(root.right);
         return Math.abs(left-right)<=1 && isBalanced(root.left) && isBalanced(root.right);
     }
+    //Q104. Maximum Depth of Binary Tree
     public int depth(TreeNode root){
         if (root==null) return 0;
         return 1 + Math.max(depth(root.left), depth(root.right));
