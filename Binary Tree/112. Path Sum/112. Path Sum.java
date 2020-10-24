e0207b1435add30347ec63d797d1bac053912af5
april-dev <66917271+april-dev@users.noreply.github.com>
2020-10-24 17:10:10 -0400

    Update 112. Path Sum.java

--- Binary Tree/112. Path Sum/112. Path Sum.java
+++ Binary Tree/112. Path Sum/112. Path Sum.java
@@ -3,3 +3,31 @@ public boolean hasPathSum(TreeNode root, int sum){
         if (root.left==null && root.right==null && sum == root.val) return true;
         return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
     }
+
+
+//Iterative
+public boolean hasPathSum(TreeNode root, int sum){
+        Stack<TreeNode> stackNode = new Stack<>();
+        Stack<Integer> stackSum = new Stack<>();
+        if (root==null) return false;
+        
+        stackNode.push(root);
+        stackSum.push(sum);
+        
+        while(!stackNode.isEmpty()){
+            TreeNode curNode = stackNode.pop();
+            int curSum = stackSum.pop();
+            
+            if (curNode.left==null && curNode.right==null && curNode.val==curSum) return true;
+            
+            if (curNode.left!=null){
+                stackNode.push(curNode.left);
+                stackSum.push(curSum - curNode.val);
+            }
+            if (curNode.right!=null){
+                stackNode.push(curNode.right);
+                stackSum.push(curSum - curNode.val);
+            }
+        }
+        return false;
+    }
