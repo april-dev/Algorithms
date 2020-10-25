96881c8de01aae22b42bb541567e3bfe8d4ee11c
april-dev <66917271+april-dev@users.noreply.github.com>
2020-10-25 15:08:38 -0400

    Update 1510. Stone Game IV.java

--- DP/1510. Stone Game IV/1510. Stone Game IV.java
+++ DP/1510. Stone Game IV/1510. Stone Game IV.java
@@ -1,3 +1,10 @@
+/*
+dp[i] = true represents that for i th number, Alice can win. False means Alice loses.
+Lets assume Alice loses for n=j.
+Thus, if at any point i Alice can remove a square number such that the remaining number is equal to j, and j is false, then Alice can win at the point i.
+*/
+
+//Time: O(n*sqrt(n))
 public boolean winnerSquareGame(int n) {
         boolean[] dp = new boolean[n+1];
         for (int i=1; i<=n; i++){
