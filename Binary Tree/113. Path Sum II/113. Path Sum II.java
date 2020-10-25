adacd05648d1db52aef59918c3d8756e8838cdad
april-dev <66917271+april-dev@users.noreply.github.com>
2020-10-25 11:42:08 -0400

    Update 113. Path Sum II.java

--- Binary Tree/113. Path Sum II/113. Path Sum II.java
+++ Binary Tree/113. Path Sum II/113. Path Sum II.java
@@ -22,3 +22,38 @@ class Solution {
         temp.remove(temp.size()-1);
     }
 }
+
+
+
+//Iterative
+public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root==null) return res;
+        List<Integer> temp =  new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        
+        int pathSum = 0;
+        TreeNode prev = null, cur = root;
+        while (cur!=null || !stack.isEmpty()){
+            while (cur != null){
+                stack.push(cur);
+                temp.add(cur.val);
+                pathSum += cur.val;
+                cur = cur.left;
+            }
+            cur = stack.peek();
+            if (cur.right != null && cur.right != prev){
+                cur = cur.right;
+                continue;
+            }
+            if (cur.left == null && cur.right == null && pathSum == sum){
+                res.add(new ArrayList<>(temp));
+            }
+            prev = cur;
+            stack.pop();
+            temp.remove(temp.size()-1);
+            pathSum -= cur.val;
+            cur = null;
+        }
+        return res;
+    }
