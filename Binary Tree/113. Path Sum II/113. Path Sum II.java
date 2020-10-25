07ea7f2145265c10745f906ffa59147c0c629f96
april-dev <66917271+april-dev@users.noreply.github.com>
2020-10-25 01:03:11 -0400

    Update 113. Path Sum II.java

--- Binary Tree/113. Path Sum II/113. Path Sum II.java
+++ Binary Tree/113. Path Sum II/113. Path Sum II.java
@@ -1,3 +1,7 @@
+/*
+Also using ArrayList allows O(1) access to the each node, that means removing the last element takes only O(1). 
+If you use LinkedList, initially we have traverse the list to the last node then remove it, which takes O(n) time. 
+*/
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
         List<List<Integer>> res = new ArrayList<>();
