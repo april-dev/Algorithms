b181c3fc166951f5b12c198ee05e1d58c1274989
april-dev <66917271+april-dev@users.noreply.github.com>
2020-10-26 00:18:50 -0400

    Update 437. Path Sum III.java

--- Binary Tree/437. Path Sum III/437. Path Sum III.java
+++ Binary Tree/437. Path Sum III/437. Path Sum III.java
@@ -39,3 +39,16 @@ public int pathSum(TreeNode root, int sum) {
         preSum.put(currSum, preSum.get(currSum) - 1);
         return res;
     }
+
+
+// Time: O(nlogn) for balanced tree, O(n^2) for worst case when tree is like linked list
+class Solution {
+    public int pathSum(TreeNode root, int sum) {
+        if (root == null) return 0;
+        return pathSumFrom(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+    }
+    public int pathSumFrom(TreeNode root, int sum){
+        if (root==null) return 0;
+       return (root.val==sum? 1:0 ) + pathSumFrom(root.left, sum - root.val) +   pathSumFrom(root.right, sum - root.val);    
+    }
+}
