bf5669b43b23f86597ab358c400ad304d086dbeb
april-dev <66917271+april-dev@users.noreply.github.com>
2020-10-26 22:44:56 -0400

    Update 129. Sum Root to Leaf Numbers.java

--- Binary Tree/129. Sum Root to Leaf Numbers/129. Sum Root to Leaf Numbers.java
+++ Binary Tree/129. Sum Root to Leaf Numbers/129. Sum Root to Leaf Numbers.java
@@ -31,3 +31,28 @@ class Solution {
     }
 }
 
+//Iterative
+public int sumNumbers(TreeNode root) {
+        if (root==null) return 0;
+        Stack<TreeNode> nodeStack = new Stack<>();
+        Stack<String> numStack = new Stack<>();
+        nodeStack.push(root);
+        numStack.push(""+root.val);
+        int res = 0;
+        while (!nodeStack.isEmpty()){
+            TreeNode curNode = nodeStack.pop();
+            String curNum = numStack.pop();
+            if (curNode.left==null && curNode.right==null){
+                res += Integer.valueOf(curNum);
+            }
+            if (curNode.left!=null){
+                nodeStack.push(curNode.left);
+                numStack.push(curNum + "" + curNode.left.val);
+            }
+            if (curNode.right!=null){
+                nodeStack.push(curNode.right);
+                numStack.push(curNum + "" + curNode.right.val);
+            }
+        }
+        return res;
+    }
