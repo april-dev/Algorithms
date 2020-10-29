86b2e26ddbcd468ab6f9d2b6b3fa833687eae8ac
april-dev <66917271+april-dev@users.noreply.github.com>
2020-10-29 16:19:45 -0400

    Create 543. Diameter of Binary Tree.java

---
+++ Binary Tree/543. Diameter of Binary Tree/543. Diameter of Binary Tree.java
@@ -0,0 +1,17 @@
+class Solution {
+    int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDiameter(root);
+        return max;
+    }
+    public int maxDiameter(TreeNode root) {
+        if (root==null) return 0;
+        
+        int left = maxDiameter(root.left);
+        int right = maxDiameter(root.right);
+        
+        max = Math.max(max, left + right);
+        
+        return Math.max(left, right) + 1;
+    }
+}
