5430a5b7b94b537bef21097c65abc3fe1fcfdc22
april-dev <66917271+april-dev@users.noreply.github.com>
2020-11-01 13:57:56 -0500

    Create 1640. Check Array Formation Through Concatenation.java

---
+++ HashMap/1640. Check Array Formation Through Concatenation/1640. Check Array Formation Through Concatenation.java
@@ -0,0 +1,15 @@
+public boolean canFormArray(int[] arr, int[][] pieces) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<arr.length; i++){
+            map.put(arr[i], i);
+        }
+        for (int[] piece: pieces){
+            if (!map.containsKey(piece[0])) return false;
+            int idx = map.get(piece[0]);
+            for (int i=1; i<piece.length; i++){
+                if (!map.containsKey(piece[i]) || map.get(piece[i])!=idx+1) return false;
+                idx++;
+            }
+        }
+        return true;
+    }
