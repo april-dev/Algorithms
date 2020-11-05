8f614f974a0053b48aaefdfd2715489c44bc835f
april-dev <66917271+april-dev@users.noreply.github.com>
2020-11-05 17:44:53 -0500

    Update 46. Permutations.java

--- Search/46. Permutations/46. Permutations.java
+++ Search/46. Permutations/46. Permutations.java
@@ -73,3 +73,35 @@ public void swap (int x, int y,int[] num){
     num[x]=num[y];
     num[y]=temp;
 } }
+
+
+//Solution 3: Iterative
+/*
+For example, if the input num[] is {1,2,3}: First, add 1 into the initial List<List<Integer>> (let's call it "answer").
+
+Then, 2 can be added in front or after 1. So we have to copy the List in answer (it's just {1}), add 2 in position 0 of {1}, 
+then copy the original {1} again, and add 2 in position 1. Now we have an answer of {{2,1},{1,2}}. There are 2 lists in the current answer.
+
+Then we have to add 3. first copy {2,1} and {1,2}, add 3 in position 0; then copy {2,1} and {1,2}, and add 3 into position 1, then do the
+same thing for position 3. Finally we have 2*3=6 lists in answer, which is what we want.
+*/
+
+public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums.length==0) return ans;
+        List<Integer> temp = new ArrayList<>();
+        temp.add(nums[0]);
+        ans.add(temp);
+        for (int i=1; i<nums.length; i++){
+            List<List<Integer>> newAns = new ArrayList<>();
+            for (List<Integer> list: ans){
+                for (int j=0; j<=i; j++){
+                    List<Integer> newList = new ArrayList<>(list);
+                    newList.add(j, nums[i]);
+                    newAns.add(new ArrayList<>(newList));
+                }
+            }
+            ans = newAns;
+        }
+        return ans;
+    }
