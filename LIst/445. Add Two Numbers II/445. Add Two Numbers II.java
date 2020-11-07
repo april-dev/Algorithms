621046051df3d591b747208811919bb7b6185bf2
april-dev <66917271+april-dev@users.noreply.github.com>
2020-11-07 15:25:15 -0500

    Update 445. Add Two Numbers II.java

--- LIst/445. Add Two Numbers II/445. Add Two Numbers II.java
+++ LIst/445. Add Two Numbers II/445. Add Two Numbers II.java
@@ -1,28 +1,27 @@
 public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack<Integer> s1 = new Stack<>();
-        Stack<Integer> s2 = new Stack<>();
+        Stack<ListNode> s1 = new Stack<>();
+        Stack<ListNode> s2 = new Stack<>();
         while (l1!=null){
-            s1.add(l1.val);
-            l1=l1.next;
+            s1.push(l1);
+            l1 = l1.next;
         }
         while (l2!=null){
-            s2.add(l2.val);
-            l2=l2.next;
+            s2.push(l2);
+            l2 = l2.next;
         }
-        
+        int carry = 0;
         ListNode dummy = null;
-        int sum =0;
-        while (!s1.isEmpty() || !s2.isEmpty() || sum!=0){
-            int v1=0, v2=0;
+        while (!s1.isEmpty() || !s2.isEmpty() || carry!=0){
+            int t1 = 0, t2 = 0;
+            if (!s1.isEmpty()) t1 = s1.pop().val;
+            if (!s2.isEmpty()) t2 = s2.pop().val;
+                       
+            int sum = t1 + t2 + carry;        
+            ListNode list = new ListNode(sum%10);
             
-            if (!s1.isEmpty()) v1=s1.pop();
-            if (!s2.isEmpty()) v2=s2.pop();
-            
-            sum= sum + v1 + v2;         
-            ListNode head = new ListNode(sum%10);
-            head.next = dummy;
-            dummy = head;
-            sum=sum/10;
+            carry = sum/10;
+            list.next = dummy;
+            dummy = list;            
         }
         return dummy;
     }
