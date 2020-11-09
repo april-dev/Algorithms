0af81fff5e3beb146d555dd0dbb100e5b4bddd67
april-dev <66917271+april-dev@users.noreply.github.com>
2020-11-09 00:17:27 -0500

    Update 695. Max Area of Island.java

--- Graph/695. Max Area of Island/695. Max Area of Island.java
+++ Graph/695. Max Area of Island/695. Max Area of Island.java
@@ -71,3 +71,81 @@ class Solution {
         return  1+ dfs(grid, i-1, j) + dfs(grid, i+1, j)+dfs(grid, i, j-1)+dfs(grid, i, j+1);       
     }
 }
+
+
+//BFS (counter version 1) variable max
+//count when added to queue
+public int maxAreaOfIsland(int[][] grid) {
+        int res = 0;
+        if (grid.length==0) return res;
+        Queue<int[]> q = new LinkedList<>();
+        int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        for (int i=0; i<grid.length; i++){
+            for (int j=0; j<grid[0].length; j++){         
+                if (grid[i][j]==1) {
+                    q.offer(new int[]{i, j});
+                    grid[i][j] = 0;
+                    int max = 1;
+                    
+                    while (!q.isEmpty()){
+                        int size = q.size();
+                        while (size-->0){
+                            int[] cur = q.remove();
+                            for (int[] dir:dirs){
+                                int x = cur[0] + dir[0];
+                                int y = cur[1] + dir[1];
+                                if (x>=0 && x<grid.length && y>=0 && y<grid[0].length && grid[x][y]==1){
+                                    grid[x][y] = 0;
+                                    max++;
+                                    q.offer(new int[]{x, y});
+                                }
+                            }
+                        }
+                    }
+                    res = Math.max(res, max);
+                }            
+            }
+        }
+        return res;
+    }
+}
+
+
+
+
+//BFS (counter version 2) variable max
+//count when pop from queue
+public int maxAreaOfIsland(int[][] grid) {
+        int res = 0;
+        if (grid.length==0) return res;
+        Queue<int[]> q = new LinkedList<>();
+        int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        for (int i=0; i<grid.length; i++){
+            for (int j=0; j<grid[0].length; j++){
+                if (grid[i][j]==1) {
+                    q.offer(new int[]{i, j});
+                    grid[i][j] = 0;
+                    int max = 0;
+                    
+                    while (!q.isEmpty()){
+                        int size = q.size();
+                        while (size-->0){
+                            int[] cur = q.remove();
+                            max++;
+                            for (int[] dir:dirs){
+                                int x = cur[0] + dir[0];
+                                int y = cur[1] + dir[1];
+                                if (x>=0 && x<grid.length && y>=0 && y<grid[0].length && grid[x][y]==1){
+                                    grid[x][y] = 0;                               
+                                    q.offer(new int[]{x, y});
+                                }
+                            }
+                        }
+                    }
+                    res = Math.max(res, max);
+                }               
+            }
+        }
+        return res;
+    }
+}
