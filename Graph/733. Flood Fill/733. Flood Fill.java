89b80d64d965aea6eae5694084dc19f126e4b3a5
april-dev <66917271+april-dev@users.noreply.github.com>
2020-11-09 18:27:15 -0500

    Update 733. Flood Fill.java

--- Graph/733. Flood Fill/733. Flood Fill.java
+++ Graph/733. Flood Fill/733. Flood Fill.java
@@ -1,4 +1,5 @@
- public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+//BFS 1
+public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         if (image[sr][sc] == newColor) return image;
         int m = image.length, n = image[0].length;
         Queue<int[]> queue = new LinkedList<>();
@@ -24,6 +25,28 @@
         }
         return image;
     }
+
+//BFS 2
+public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int oldColor = image[sr][sc];
+        if (newColor==oldColor) return image;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{sr, sc});
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        while (!q.isEmpty()){
+            int[] cur = q.remove();
+         //update color when it is popped from the queue
+            image[cur[0]][cur[1]] = newColor;
+            for (int[] dir:dirs){
+                int x = cur[0] + dir[0];
+                int y = cur[1] + dir[1];
+                if (x>=0 && x<image.length && y>=0 && y<image[0].length && image[x][y]==oldColor){
+                    q.add(new int[]{x, y});
+                }                
+            }
+        }       
+        return image;
+    } 
    
 //DFS
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
