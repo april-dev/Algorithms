8d8abddd3e0ce7b1113a3c697b6a4eccd6f221d9
april-dev <66917271+april-dev@users.noreply.github.com>
2020-11-13 17:28:44 -0500

    Update 127. Word Ladder.java

--- Search/127. Word Ladder/127. Word Ladder.java
+++ Search/127. Word Ladder/127. Word Ladder.java
@@ -5,6 +5,11 @@ avoid visited words is necessary. see example below.
 ["hot","dot","dog","lot","log"]
 */
 
+/*
+Time O(nL), n is the number of the words in the wordList, and L is the length of the each word.
+Because, the worst case is that each word needs to enqueue and dequeue. And each time the we need to loop of L times, 
+to check if it exits in the wordList. And the find function is O(1) and to change number is O(1). so the finally time complexity is O(nL).
+*/
 public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> dict = new HashSet<>(wordList); //, vis = new HashSet<>();
         Queue<String> q = new LinkedList<>();
