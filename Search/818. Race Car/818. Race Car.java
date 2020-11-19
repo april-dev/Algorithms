3ca7fbf85cc2c0cf4fcdef641a474fab8670e8c3
april-dev <66917271+april-dev@users.noreply.github.com>
2020-11-19 16:21:04 -0500

    Update 818. Race Car.java

--- Search/818. Race Car/818. Race Car.java
+++ Search/818. Race Car/818. Race Car.java
@@ -1,3 +1,16 @@
+//Time O(2^n) for n instructions
+/*
+We should allow the car to be able to pass the target and then reverse the direction to go to the target. However, there should be an upper bound because there's no point to 
+keep accelerating after passing the target.
+
+To find the upper bound, lets' consider an extreme case where the car is always accelerating, the number of acceleration is m, the car with position (2^m) - 1 has not 
+reached the target: (2^m)-1 < target. Since the target is an integer, we can rewrite it as: target = (2^m)+k, k >= 0... (1)
+
+In the above extreme case, the car's speed 2^m is the maximum possible speed at the position (2^m)-1. Note that we should allow the car to be able to pass the target, 
+which leads to the maximum next position is (2^m)-1 + 2^m... (2)
+
+Combine (1) and (2) we get (2^m)-1+2^m = 2*(2^m)-1 < 2*(2^m)+2*k = 2*target, k >= 0. It says, the upper bound for any position with any possible speed is 2*target
+*/
 public int racecar(int target) {
         Queue<int[]> queue = new LinkedList<>();
         HashSet<String> visited = new HashSet<>();
