839f6fd4a65c8c3a61cc5ed5f42e86ecda0ccfb7
april-dev <66917271+april-dev@users.noreply.github.com>
2020-11-23 17:59:59 -0500

    Create 131. Palindrome Partitioning.java

---
+++ Search/131. Palindrome Partitioning/131. Palindrome Partitioning.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        List<String> temp = new ArrayList<>();
+        backtrack(s, 0, temp, res);
+        return res;
+    }
+    public void backtrack(String s, int index, List<String> temp, List<List<String>> res){
+        if (index==s.length()){
+            res.add(new ArrayList<>(temp));
+            return;
+        }       
+        for (int i=1; i<=s.length(); i++){
+            if (index+i>s.length()) break;
+            String cur = s.substring(index, index+i);
+            if (!isValid(cur)) continue;
+            temp.add(cur);
+            backtrack(s, index+i, temp, res);
+            temp.remove(temp.size()-1);
+        }
+    }
+    public boolean isValid(String s){
+        int i=0, j=s.length()-1;
+        while(i<j){
+            if (s.charAt(i)!=s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
