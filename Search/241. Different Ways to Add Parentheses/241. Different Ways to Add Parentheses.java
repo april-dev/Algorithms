dcaa133300471113ef3cb2a4a4b48e8b5c4172f2
april-dev <66917271+april-dev@users.noreply.github.com>
2020-11-24 15:36:15 -0500

    Update 241. Different Ways to Add Parentheses.java

--- Search/241. Different Ways to Add Parentheses/241. Different Ways to Add Parentheses.java
+++ Search/241. Different Ways to Add Parentheses/241. Different Ways to Add Parentheses.java
@@ -27,3 +27,39 @@ class Solution {
         return ret;
     }
 }
+
+
+//using map to reduce the time to do repeated computation
+class Solution {
+    Map<String, List<Integer>> map = new HashMap<>();
+    
+    public List<Integer> diffWaysToCompute(String input) {
+        if (map.containsKey(input)) return map.get(input);
+        List<Integer> ret = new ArrayList<>();
+        for (int i=0; i<input.length(); i++){
+            if (input.charAt(i)=='*' || input.charAt(i)=='-' || input.charAt(i)=='+'){
+                String left = input.substring(0, i);
+                String right = input.substring(i+1);
+                List<Integer> leftSet = diffWaysToCompute(left);
+                List<Integer> rightSet = diffWaysToCompute(right);
+                for (int p1:leftSet){
+                    for (int p2:rightSet){
+                        int c = 0;
+                        switch (input.charAt(i)){
+                            case '+': c = p1 + p2;
+                                break;
+                            case '-': c = p1 - p2;
+                                break;
+                            case '*': c = p1 * p2;
+                                break;
+                        }
+                        ret.add(c);
+                    }
+                }
+            }
+        }
+        if (ret.size()==0) ret.add(Integer.valueOf(input));
+        map.put(input, ret);
+        return ret;
+    }
+}
