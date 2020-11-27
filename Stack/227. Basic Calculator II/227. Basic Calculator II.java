53cb06b22004308df6aab9cdbc3bacba07739ce1
april-dev <66917271+april-dev@users.noreply.github.com>
2020-11-27 18:28:41 -0500

    Update 227. Basic Calculator II.java

--- Stack/227. Basic Calculator II/227. Basic Calculator II.java
+++ Stack/227. Basic Calculator II/227. Basic Calculator II.java
@@ -1,4 +1,35 @@
 public int calculate(String s) {
+        int num = 0;
+        Stack<Integer> stack = new Stack<>();
+        char sign = '+';
+        for (int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) num = num*10 + c - '0';
+            if (i==s.length()-1 || !Character.isDigit(c) && c != ' '){
+                switch (sign){
+                    case '+':
+                        stack.push(num);
+                        break;
+                    case '-':
+                        stack.push(-num);
+                        break;
+                    case '*':
+                        stack.push(stack.pop()*num);
+                        break;
+                    case '/':
+                        stack.push(stack.pop()/num);
+                }
+                sign= c;
+                num = 0;
+            }
+        }
+        int sum = 0;
+        for (int i:stack) sum += i;
+        return sum;
+    }
+
+
+public int calculate(String s) {
         int sum = 0;
         int tempSum = 0;
         int num = 0;
