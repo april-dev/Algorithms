8d60104172301ac6f679eb9443fc0a8fbe60d26a
april-dev <66917271+april-dev@users.noreply.github.com>
2020-11-28 17:52:37 -0500

    Update 239. Sliding Window Maximum.java

--- Stack/239. Sliding Window Maximum/239. Sliding Window Maximum.java
+++ Stack/239. Sliding Window Maximum/239. Sliding Window Maximum.java
@@ -1,17 +1,13 @@
 public int[] maxSlidingWindow(int[] nums, int k) {
-        if (nums==null || k<=0 ) return new int[0];
         int n = nums.length;
+        if (n==0) return nums;
         int[] res = new int[n-k+1];
-        Deque<Integer> deque = new ArrayDeque<>();
-        int ri = 0;
-        for (int i=0; i<n; i++){
-            if (!deque.isEmpty() && deque.peek()<i-k+1) deque.poll();
-            while (!deque.isEmpty() &&  nums[i]>nums[deque.peekLast()]) deque.pollLast();
-            deque.offer(i);
-            if (i>=k-1) {
-                res[ri] = nums[deque.peek()];
-                ri+=1;
-            }
+        LinkedList<Integer> dq = new LinkedList<>();
+        for (int i=0; i<nums.length; i++){
+            if (!dq.isEmpty() && dq.peek() < i-k+1) dq.poll();
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) dq.pollLast();
+            dq.offer(i);
+            if (i >= k-1) res[i-k+1] = nums[dq.peek()];
         }
-        return res;    
+        return res;
     }
