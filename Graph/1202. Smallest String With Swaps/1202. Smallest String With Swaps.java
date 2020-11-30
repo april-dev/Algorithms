aa0fc77a0d3e67a0010e9d8511ebced1690677f3
april-dev <66917271+april-dev@users.noreply.github.com>
2020-11-30 00:17:44 -0500

    Update 1202. Smallest String With Swaps.java

--- Graph/1202. Smallest String With Swaps/1202. Smallest String With Swaps.java
+++ Graph/1202. Smallest String With Swaps/1202. Smallest String With Swaps.java
@@ -58,38 +58,48 @@
         return parent[a];
     }
 
-//DFS approach
-public:
-    vector<int> indices;                                                 //Stores indices of same group.
-    vector<bool> visited;
-    vector<vector<int>> adjList;
-    string indiceString;                                                 //Stores  string formed by indices in the same group.
-    void dfs(string &s,int n)                                             //DFS to get all indices in same group.
-    {
-        visited[n]=true;
-        indices.push_back(n);
-        indiceString+=s[n];
-        for(int &i:adjList[n])
-            if(!visited[i])
-               dfs(s,i);
+//DFS
+class Solution {
+    List<Character> indiceString = new ArrayList<>();
+    List<Integer> indices = new ArrayList<>();
+    boolean[] visited;
+    HashMap<Integer, List<Integer>> map;
+    
+    public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
+        map = new HashMap<>();
+        visited = new boolean[s.length()];
+        for (List<Integer> l:pairs){
+            map.putIfAbsent(l.get(0), new ArrayList<>());
+            map.putIfAbsent(l.get(1), new ArrayList<>());
+            map.get(l.get(0)).add(l.get(1));
+            map.get(l.get(1)).add(l.get(0));
+        }
+        
+        char[] str = s.toCharArray();
+        for (int i=0; i<s.length(); i++){
+            if (visited[i]==true) continue;
+            indiceString.clear();
+            indices.clear();
+            
+            dfs(s, i);
+            
+            Collections.sort(indices);
+            Collections.sort(indiceString);
+            for (int j=0; j<indices.size(); j++){
+                str[indices.get(j)] = indiceString.get(j);
+            }            
+        }
+        return new String(str);
     }
-    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) 
-    {
-        adjList.resize(s.length());
-        visited.resize(s.length(),false);
-        for(vector<int> &v:pairs)                               //Create adjacency list using the indice pairs
-            adjList[v[0]].push_back(v[1]),adjList[v[1]].push_back(v[0]);
-        for(int i=0;i<s.length();i++)
-            if(!visited[i])
-            {
-                indiceString="";                              //Clear string formed by one group of indices before finding next group.
-                indices.clear();                             //Clear indices vector before finding another group.
-                dfs(s,i);
-                sort(indiceString.begin(),indiceString.end());                    //Sort the characters in the same group.
-                sort(indices.begin(),indices.end());                                  //Sort the indices in the same group.            
-                for(int i=0;i<indices.size();i++)          //Replace all the indices in the same group with the sorted characters.
-                    s[indices[i]]=indiceString[i];
+    public void dfs(String s, int i){
+        visited[i] = true;
+        indices.add(i);
+        indiceString.add(s.charAt(i));
+        if (map.containsKey(i)){
+            for (int cur: map.get(i)){
+                if (visited[cur]==true) continue;
+                dfs(s, cur);
             }
-        return s;
-    }
-
+        }
+    }    
+}
