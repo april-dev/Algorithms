913d1c83e1495e1d9d69aaf511d78bd0af8cfc6f
april-dev <66917271+april-dev@users.noreply.github.com>
2020-12-03 16:58:50 -0500

    Update 990. Satisfiability of Equality Equations.java

--- Graph/990. Satisfiability of Equality Equations/990. Satisfiability of Equality Equations.java
+++ Graph/990. Satisfiability of Equality Equations/990. Satisfiability of Equality Equations.java
@@ -1,16 +1,34 @@
-int[] uf = new int[26];
+class Solution {
     public boolean equationsPossible(String[] equations) {
-        for (int i = 0; i < 26; ++i) uf[i] = i;
-        for (String e : equations)
-            if (e.charAt(1) == '=')
-                uf[find(e.charAt(0) - 'a')] = find(e.charAt(3) - 'a');
-        for (String e : equations)
-            if (e.charAt(1) == '!' && find(e.charAt(0) - 'a') == find(e.charAt(3) - 'a'))
-                return false;
+        int[] parent = new int[26];
+        for (int i=0; i<26; i++) parent[i] = i;
+        for (String s:equations){
+            int a = s.charAt(0) - 'a';
+            int b = s.charAt(3) - 'a';
+            if (s.charAt(1)=='=' && s.charAt(2)=='='){
+                union(parent, a, b);
+            }
+        }
+        for (String s:equations){
+            int a = s.charAt(0) - 'a';
+            int b = s.charAt(3) - 'a';
+            if (s.charAt(1)=='!' && s.charAt(2)=='='){
+                int p1 = find(parent, a);
+                int p2 = find(parent, b);
+                if (p1==p2) return false;
+            }
+        }
         return true;
     }
-
-    public int find(int x) {
-        if (x != uf[x]) uf[x] = find(uf[x]);
-        return uf[x];
+    public int find(int[] parent, int i){
+        if (parent[i] != i){
+            parent[i] = find(parent, parent[i]);
+        }
+        return parent[i];
     }
+    public void union(int[] parent, int a, int b){
+        int p1 = find(parent, a);
+        int p2 = find(parent, b);
+        if (p1!=p2) parent[p2] = p1;
+    }
+}
