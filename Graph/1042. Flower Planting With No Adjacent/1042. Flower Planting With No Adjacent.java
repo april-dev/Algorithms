84ccede37071100683a5bb55609fa26678fd8d57
april-dev <66917271+april-dev@users.noreply.github.com>
2020-12-05 17:52:33 -0500

    Update 1042. Flower Planting With No Adjacent.java

--- Graph/1042. Flower Planting With No Adjacent/1042. Flower Planting With No Adjacent.java
+++ Graph/1042. Flower Planting With No Adjacent/1042. Flower Planting With No Adjacent.java
@@ -1,4 +1,29 @@
-    public int[] gardenNoAdj(int N, int[][] paths) {
+//my solution
+class Solution {
+    public int[] gardenNoAdj(int n, int[][] paths) {
+        HashMap<Integer, List<Integer>> graph = new HashMap<>();
+        for (int i=0; i<n; i++) graph.put(i, new ArrayList<>());
+        for (int[] path:paths){
+            graph.get(path[0]-1).add(path[1]-1);
+            graph.get(path[1]-1).add(path[0]-1); 
+        }
+        int[] res = new int[n];
+        for (int i=0; i<n; i++){
+            int[] color = new int[5];
+            for (int child:graph.get(i)){
+                color[res[child]] = 1;
+            }
+            for (int c=1; c<=4; c++){
+                if (color[c]==0) res[i] = c;
+            }
+        }
+        return res;
+    }
+}
+
+
+
+public int[] gardenNoAdj(int N, int[][] paths) {
         Map<Integer, Set<Integer>> G = new HashMap<>();
         for (int i = 0; i < N; i++) G.put(i, new HashSet<>());
         for (int[] p : paths) {
