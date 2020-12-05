f47b008969fd203c7c700d1b4bcc999a00e9436f
april-dev <66917271+april-dev@users.noreply.github.com>
2020-12-05 18:14:26 -0500

    Update 997. Find the Town Judge.java

--- Graph/997. Find the Town Judge/997. Find the Town Judge.java
+++ Graph/997. Find the Town Judge/997. Find the Town Judge.java
@@ -1,4 +1,22 @@
- public int findJudge(int N, int[][] trust) {
+//My solution
+public int findJudge(int N, int[][] trust) {
+        int[] inDegree = new int[N+1];
+        int[] outDegree = new int[N+1];
+        for (int[] pair: trust){
+            int p1 = pair[0];
+            int p2 = pair[1];
+            inDegree[p2]++;
+            outDegree[p1]++;
+        }
+        for (int i=1; i<=N; i++){
+            if (inDegree[i]==N-1 && outDegree[i]==0) return i;
+        }
+        return -1;
+    }
+
+
+//solution from Leetcode
+public int findJudge(int N, int[][] trust) {
         int[] count = new int[N+1];
         for (int[] t:trust){
             count[t[0]]--;
