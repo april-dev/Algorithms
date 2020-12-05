355e1446972f8a2a4e7f737a8733673bdf0d7a9e
april-dev <66917271+april-dev@users.noreply.github.com>
2020-12-05 15:42:56 -0500

    Update 886. Possible Bipartition.java

--- Graph/886. Possible Bipartition/886. Possible Bipartition.java
+++ Graph/886. Possible Bipartition/886. Possible Bipartition.java
@@ -1,3 +1,40 @@
+//My solution
+//use boolean array instead of hashmap can save some space
+//Time: O(V + E)
+
+class Solution {
+    public boolean possibleBipartition(int N, int[][] dislikes) {
+        int[] states = new int[N+1];
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        for (int[] dislike: dislikes){
+            int d1 = dislike[0];
+            int d2 = dislike[1];
+            map.putIfAbsent(d1, new ArrayList<>());
+            map.putIfAbsent(d2, new ArrayList<>());
+            map.get(d1).add(d2);
+            map.get(d2).add(d1);
+        }
+        for (int i=1; i<=N; i++){
+            if (states[i]==0){
+                if (dfs(i, 1, states, map)==false) return false;
+            }
+        }
+        return true;
+    }
+    public boolean dfs(int node, int state, int[] states, HashMap<Integer, List<Integer>> map){
+        if (states[node]!=0) return states[node]==state;
+        states[node] = state;
+        if (map.containsKey(node)){
+            for (int child: map.get(node)){
+                if (dfs(child, -state, states, map)==false) return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+//solution from Leetcode
 public boolean possibleBipartition(int N, int[][] dislikes) {
         boolean[][] g = new boolean[N][N];
         for (int[] d : dislikes) {
