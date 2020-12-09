5fa0aa91af504c9f5c545d958df46413b9e3f038
april-dev <66917271+april-dev@users.noreply.github.com>
2020-12-08 23:35:37 -0500

    Update 1129. Shortest Path with Alternating Colors.java

--- Graph/1129. Shortest Path with Alternating Colors/1129. Shortest Path with Alternating Colors.java
+++ Graph/1129. Shortest Path with Alternating Colors/1129. Shortest Path with Alternating Colors.java
@@ -1,44 +1,53 @@
-//1=red, 2=blue, 0=root-edge (special case)
-public int[] shortestAlternatingPaths(int n, int[][] red_edges, int[][] blue_edges) {
-        List<Integer>[] reds = new ArrayList[n], blues = new ArrayList[n];
-        for(int[] e : red_edges){
-            if(reds[e[0]] == null)
-                reds[e[0]] = new ArrayList<>();
-            reds[e[0]].add(e[1]);
+class Solution {
+   public int[] shortestAlternatingPaths(int n, int[][] red_edges, int[][] blue_edges) {
+       int[][] graph = new int[n][n];
+       buildGraph(graph, n, red_edges, blue_edges);
+       Queue<int[]> queue = new LinkedList<>();
+       HashSet<String> visited = new HashSet<>();
+       queue.offer(new int[]{0, 1});
+       queue.offer(new int[]{0, -1});
+       visited.add(0 + "," + 1);
+       visited.add(0 + "," + "-1");
+       int[] res = new int[n];
+       Arrays.fill(res, Integer.MAX_VALUE);
+       res[0] = 0;
+       int len = 1;
+       while(!queue.isEmpty()){
+           int size = queue.size();
+           while (size-->0){
+               int[] cur = queue.poll();
+               int node = cur[0];
+               int color = cur[1];
+              //for (int i=1; i<n; i++){  //if i start from 1, then line 9 and 10 are not necessary   
+               for (int i=0; i<n; i++){
+                   if (graph[node][i] == color || graph[node][i] ==0){
+                   if (!visited.add(i + "," + color)) continue;
+                   queue.offer(new int[]{i, -color});
+                   res[i] = Math.min(res[i], len);
+                   }
+               }
+           }
+           len++;
+       }
+       for (int i=0; i<n; i++){
+           if (res[i]==Integer.MAX_VALUE) res[i] = -1;
+       }
+       return res;
+   }
+    public void buildGraph(int[][] graph, int n, int[][] red_edges, int[][] blue_edges){
+        for (int i=0; i<n; i++) Arrays.fill(graph[i], -n);
+        //red path is marked as 1, blue path is marked as -1, if the path appears in both red and blue, then marked as 0
+        for (int[] red: red_edges){
+            graph[red[0]][red[1]] = 1;
         }
-        for(int[] e : blue_edges){
-            if(blues[e[0]] == null)
-                blues[e[0]] = new ArrayList<>();
-            blues[e[0]].add(e[1]);
+        for (int[] blue: blue_edges){
+            int u = blue[0];
+            int v = blue[1];
+            if (graph[u][v]==1) graph[u][v] = 0;
+            else graph[u][v] = -1;
         }
-        Queue<int[]> q = new LinkedList<>();
-        int[] res = new int[n];
-        Arrays.fill(res, -1);
-        q.add(new int[]{0, 0});
-        int moves = 0;
-        Set<String> seen = new HashSet<>();
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i = 0; i < size; i++){
-                int[] curr = q.remove();
-                String key = curr[0]+" "+curr[1];
-                if(seen.contains(key)) continue;
-                seen.add(key);
-                if(res[curr[0]] == -1)
-                    res[curr[0]] = moves;
-                if(curr[1] == 2 || curr[1] == 0)
-                    if(reds[curr[0]] != null)
-                        for(int child : reds[curr[0]])
-                            q.add(new int[]{child, 1});
-                if(curr[1] == 1 || curr[1] == 0)
-                    if(blues[curr[0]] != null)
-                        for(int child : blues[curr[0]])
-                            q.add(new int[]{child, 2});
-            }
-            ++moves;
-        }
-        return res;
     }
+}
     
     
     
