8de11e802cf0c27ca7da187ab472f4b5c96b1e38
april-dev <66917271+april-dev@users.noreply.github.com>
2020-12-20 23:50:30 -0500

    Update 1696. Jump Game VI.java

--- Stack/1696. Jump Game VI/1696. Jump Game VI.java
+++ Stack/1696. Jump Game VI/1696. Jump Game VI.java
@@ -17,3 +17,23 @@ public int maxResult(int[] nums, int k) {
         }
         return dp[nums.length-1];
     }
+
+
+//Small improvement
+// i start from 0 in the for loop
+public int maxResult(int[] nums, int k) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, Integer.MIN_VALUE);
+        dp[0] = nums[0];
+        
+        LinkedList<Integer> dq = new LinkedList<>();
+       // dq.add(0);
+        for (int i=0; i<nums.length; i++){
+            int max = dq.isEmpty()? 0 : dp[dq.peek()];        
+            dp[i] = max+nums[i];
+            if (!dq.isEmpty() && dq.peek() < i-k+1) dq.poll();
+            while (!dq.isEmpty() && dp[dq.peekLast()] < dp[i]) dq.pollLast();           
+            dq.offer(i);           
+        }
+        return dp[nums.length-1];
+    }
