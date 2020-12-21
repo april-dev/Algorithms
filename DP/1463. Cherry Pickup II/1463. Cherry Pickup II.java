d9cc3c37b4c771a93b02de6ad80c202fb54aa7d2
april-dev <66917271+april-dev@users.noreply.github.com>
2020-12-21 13:57:08 -0500

    Update 1463. Cherry Pickup II.java

--- DP/1463. Cherry Pickup II/1463. Cherry Pickup II.java
+++ DP/1463. Cherry Pickup II/1463. Cherry Pickup II.java
@@ -22,3 +22,40 @@ class Solution {
         return dp[r][c1][c2];
     }
 }
+
+
+
+//bottom up
+//initialize dp to 0 is wrong, consider[[0, 0, 0, 10, 0, 0, 0], [0, 0, 0, 10, 0, 0, 0]], answer should be 0, the middle ones wont be reached.
+
+public int cherryPickup(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[][][] dp = new int[m][n][n];
+        for (int i=0; i<m; i++){
+            for (int j=0; j<n; j++){
+                Arrays.fill(dp[i][j], -1);
+            }
+        }
+        dp[0][0][n-1] = grid[0][0] + grid[0][n-1];
+        for (int i=1; i<m; i++){
+            for (int j=0; j<n; j++){
+                for (int k=0; k<n; k++){
+                    int cherries = j==k? grid[i][j]:(grid[i][j] + grid[i][k]);
+                    for (int p=-1; p<=1; p++){
+                        for (int q=-1; q<=1; q++){
+                            int c1 = j+p;
+                            int c2 = k+q;
+                            if (c1>=0 && c1<n && c2>=0 && c2<n && dp[i-1][c1][c2] != -1) dp[i][j][k] = Math.max(dp[i][j][k], cherries+ dp[i-1][c1][c2]);
+                        }
+                    }
+                }
+            }
+        }
+        int res = 0;
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                res = Math.max(res, dp[m-1][i][j]);
+            }
+        }
+        return res;
+    }
