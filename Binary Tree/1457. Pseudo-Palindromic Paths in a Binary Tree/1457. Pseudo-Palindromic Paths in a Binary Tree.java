1bd60295f3da8f5f3bd03b09e0659f0fc2c4bcd8
april-dev <66917271+april-dev@users.noreply.github.com>
2020-12-29 12:40:55 -0500

    Update 1457. Pseudo-Palindromic Paths in a Binary Tree.java

--- Binary Tree/1457. Pseudo-Palindromic Paths in a Binary Tree/1457. Pseudo-Palindromic Paths in a Binary Tree.java
+++ Binary Tree/1457. Pseudo-Palindromic Paths in a Binary Tree/1457. Pseudo-Palindromic Paths in a Binary Tree.java
@@ -25,3 +25,39 @@ class Solution {
         count[root.val]--;
     }    
 }
+
+
+//solution from leetcode
+public int pseudoPalindromicPaths (TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode root, int count) {
+        if (root == null) return 0;
+        count ^= 1 << (root.val - 1);
+        int res = dfs(root.left, count) + dfs(root.right, count);
+        if (root.left == root.right && (count & (count - 1)) == 0) res++;
+        return res;
+    }
+
+
+//solution using hashset
+public int pseudoPalindromicPaths (TreeNode root) {
+        return canBePalindrome(root,new HashSet());
+    }
+    
+    private int canBePalindrome(TreeNode node,Set<Integer> numbers){
+        if(node==null) return 0;
+        if(numbers.contains(node.val)){
+            numbers.remove(node.val);
+        }else{
+            numbers.add(node.val);
+        }
+        if(node.left==null&& node.right==null){
+            return numbers.size()<=1?1:0; // thanks to @MananS77
+        }
+        int left=canBePalindrome(node.left,new HashSet(numbers));
+        int right=canBePalindrome(node.right,new HashSet(numbers));
+        return left+right;
+    }
+
