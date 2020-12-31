dd8362f0770144992cf28a9b8c5656300ad09fe2
april-dev <66917271+april-dev@users.noreply.github.com>
2020-12-31 00:16:05 -0500

    Update 33. Search in Rotated Sorted Array.java

--- Binary Search/33. Search in Rotated Sorted Array/33. Search in Rotated Sorted Array.java
+++ Binary Search/33. Search in Rotated Sorted Array/33. Search in Rotated Sorted Array.java
@@ -1,6 +1,27 @@
 
 // see 81 for duplicates
 
+//same template as Q81
+public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right){
+            int mid = left + (right - left)/2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid]<=nums[right]){
+                if (target > nums[mid] && target <=nums[right]) left = mid + 1;
+                else right = mid - 1;
+                    
+            }
+            else{
+                if (target >= nums[left] && target < nums[mid]) right = mid - 1;
+                else left = mid + 1;
+                    
+            }
+        }
+        return -1;
+    }
+
+//alternative solution
 public int search(int[] nums, int target) {
        if (nums == null || nums.length == 0) return -1;
         
