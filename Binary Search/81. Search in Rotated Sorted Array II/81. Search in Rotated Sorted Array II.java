25f18d8d1a3268876d5c5cd85b5c992b72c5ecf0
april-dev <66917271+april-dev@users.noreply.github.com>
2020-12-31 00:12:14 -0500

    Update 81. Search in Rotated Sorted Array II.java

--- Binary Search/81. Search in Rotated Sorted Array II/81. Search in Rotated Sorted Array II.java
+++ Binary Search/81. Search in Rotated Sorted Array II/81. Search in Rotated Sorted Array II.java
@@ -1,68 +1,38 @@
 //Compare with Q33 (no duplicates)
-//consider case [1,1,1,1,2,1], nums[left] == nums[mid] == nums[right], therefore will pass the first for loop in Q33. 
-
-public boolean search(int[] nums, int target) {
-    int start  = 0, end = nums.length - 1;
-    
-    //check each num so we will check start == end
-    //We always get a sorted part and a half part
-    //we can check sorted part to decide where to go next
-    while(start <= end){
-        int mid = start + (end - start)/2;
-        if(nums[mid] == target) return true;
-        
-        //if left part is sorted
-        if(nums[start] < nums[mid]){
-            if(target < nums[start] || target > nums[mid]){
-                //target is in rotated part
-                start = mid + 1;
-            }else{
-                end = mid - 1;
-            }
-        }else if(nums[start] > nums[mid]){
-            //right part is rotated
-            
-            //target is in rotated part
-            if(target < nums[mid] || target > nums[end]){
-                end = mid -1;
-            }else{
-                start = mid + 1;
-            }
-        }else{
-            //duplicates, we know nums[mid] != target, so nums[start] != target
-            //based on current information, we can only move left pointer to skip one cell
-            //thus in the worest case, we would have target: 2, and array like 11111111, then
-            //the running time would be O(n)
-            start ++;
-        }
-    }
-    
-    return false;
-}
+//consider case [1,1,1,1,2,1], nums[left] == nums[mid] == nums[right], therefore will not pass the first for loop in Q33. 
 
 
-//alternative method
+//The idea is the same as the previous one without duplicates
+/*
+The only difference is that due to the existence of duplicates, we can have nums[left] == nums[mid] and in that case, 
+the first half could be out of order (i.e. NOT in the ascending order, e.g. [3 1 2 3 3 3 3]) and we have to deal this case separately. 
+In that case, it is guaranteed that nums[right] also equals to nums[mid], so what we can do is to check if nums[mid]== nums[left] == nums[right] 
+before the original logic, and if so, we can move left and right both towards the middle by 1. and repeat.
+/*
+
 public boolean search(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
-        if (nums == null || nums.length == 0) return false;
-        while (left<=right){
-            int mid = left + (right-left)/2;
-            if (nums[mid]==target) return true;
-            if (nums[mid]<nums[left]){
-                if (target>= nums[mid] && target<= nums[right]){
-                    left = mid;
-                }else{
-                    right = mid;
-                }
-            }else if (nums[mid]>nums[left]){
-                if (target<= nums[mid] && target>= nums[left]){
-                    right = mid;
-                }else{
-                    left = mid;
-                }
-            }else{
+        while (left <= right){
+            int mid = left + (right - left)/2;
+            if (nums[mid] == target) return true;
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]){
                 left++;
+                right--;
+            }
+            else if (nums[mid]<=nums[right]){
+                if (target > nums[mid] && target <=nums[right]) left = mid + 1;
+                else right = mid - 1;
+                    
+            }
+            else{
+                if (target >= nums[left] && target < nums[mid]) right = mid - 1;
+                else left = mid + 1;
+                    
             }
         }
         return false;
     }
+
+
+
+
