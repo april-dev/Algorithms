cf51823d6a43e36388cc15f8b934df6386f265ee
april-dev <66917271+april-dev@users.noreply.github.com>
2021-01-03 02:05:07 -0500

    Update 1711. Count Good Meals.java

--- HashMap/1711. Count Good Meals/1711. Count Good Meals.java
+++ HashMap/1711. Count Good Meals/1711. Count Good Meals.java
@@ -1,3 +1,21 @@
+//my solution
+public int countPairs(int[] deliciousness) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        int mod = 1000000007;
+        for (int d: deliciousness){
+            if (map.containsKey(d)) {
+                count += map.get(d);
+                count %= mod;
+            }
+            for (int j=0; j<=21; j++){
+                map.put((1<<j) - d, map.getOrDefault((1<<j) - d, 0) + 1);
+            }
+        }
+        return count;
+    }
+
+
 public int countPairs(int[] deliciousness) {
         HashMap<Integer, Integer> map = new HashMap<>();
         int count = 0;
