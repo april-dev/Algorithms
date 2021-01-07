36af9735c449b0aa14f606644668cc14f28c8c31
april-dev <66917271+april-dev@users.noreply.github.com>
2021-01-07 18:27:20 -0500

    Update 3. Longest Substring Without Repeating Characters.java

--- Two Pointers/3. Longest Substring Without Repeating Characters/3. Longest Substring Without Repeating Characters.java
+++ Two Pointers/3. Longest Substring Without Repeating Characters/3. Longest Substring Without Repeating Characters.java
@@ -5,6 +5,11 @@ public int lengthOfLongestSubstring(String s) {
         int max=0;
         for (int i=0, j=0; i<s.length(); ++i){
             if (map.containsKey(s.charAt(i))){
+                /*
+                if input="abbabc"
+                when i = 3, s.chatAt(3) == 'a' , we will found last 'a' appears at index 0,
+                but we should not update j from 2 ('b') to 0 ('a'), because here although the 'a' is in hashMap, but it appears before 'b'.              
+                */
                 j = Math.max(j,map.get(s.charAt(i))+1);
             }
             map.put(s.charAt(i),i);
