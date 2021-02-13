87df664431e3073aa7016dbac12023ed776f4e29
april-dev <66917271+april-dev@users.noreply.github.com>
2021-02-13 14:32:02 -0500

    Update 120. Triangle.java

--- DP/120. Triangle/120. Triangle.java
+++ DP/120. Triangle/120. Triangle.java
@@ -12,3 +12,17 @@ public int minimumTotal(List<List<Integer>> triangle) {
          }
         return dp[0];
     }
+
+//my solution
+public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[triangle.get(n-1).size()];
+        for (int i=0; i<dp.length; i++){
+            dp[i] = triangle.get(n-1).get(i);
+        }
+        for (int i=n-2; i>=0; i--){
+            for (int j=0; j<=i; j++)
+            dp[j] = Math.min(dp[j], dp[j+1]) + triangle.get(i).get(j);
+        }
+        return dp[0];
+    }
