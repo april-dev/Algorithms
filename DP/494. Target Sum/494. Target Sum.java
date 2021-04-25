341c3982cfce425f382652b65782cc0b3b9c8693
april-dev <66917271+april-dev@users.noreply.github.com>
2021-04-25 16:04:21 -0400

    Update 494. Target Sum.java

--- DP/494. Target Sum/494. Target Sum.java
+++ DP/494. Target Sum/494. Target Sum.java
@@ -1,3 +1,4 @@
+//DP
 public int findTargetSumWays(int[] nums, int S) {
         int sum = 0;
         for (int n:nums) sum+=n;
@@ -14,7 +15,31 @@ public int findTargetSumWays(int[] nums, int S) {
         return dp[nums.length][sum+S];
     }
 
-
+//DFS with memorization
+public int findTargetSumWays(int[] nums, int S) {
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+        return helper(nums, 0, 0, S, new HashMap<>());
+    }
+    private int helper(int[] nums, int index, int sum, int S, Map<String, Integer> map){
+        String encodeString = index + "->" + sum;
+        if (map.containsKey(encodeString)){
+            return map.get(encodeString);
+        }
+        if (index == nums.length){
+            if (sum == S){
+                return 1;
+            }else {
+                return 0;
+            }
+        }
+        int curNum = nums[index];
+        int add = helper(nums, index + 1, sum - curNum, S, map);
+        int minus = helper(nums, index + 1, sum + curNum, S, map);
+        map.put(encodeString, add + minus);
+        return add + minus;
+    }
 //O(2^N)
 class Solution {
     public int findTargetSumWays(int[] nums, int S) {
