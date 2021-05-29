39ff57b231b456cbcd97d641208a7e5af6e0c0db
april-dev <66917271+april-dev@users.noreply.github.com>
2021-05-28 23:19:55 -0400

    Create 898. Bitwise ORs of Subarrays.java

---
+++ Bit Manipulation/898. Bitwise ORs of Subarrays/898. Bitwise ORs of Subarrays.java
@@ -0,0 +1,11 @@
+public int subarrayBitwiseORs(int[] arr) {
+        Set<Integer> res = new HashSet<>(), prev = new HashSet<>();
+        for (int num : arr) {
+            Set<Integer> cur = new HashSet<>();
+            cur.add(num);
+            for (int old : prev) cur.add(old | num);
+            res.addAll(cur);
+            prev = cur;
+        }
+        return res.size();
+    }
