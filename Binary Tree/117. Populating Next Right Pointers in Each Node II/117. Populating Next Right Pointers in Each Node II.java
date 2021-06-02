bf094098cb5bc200f7188d5e49020fce64af6daf
april-dev <66917271+april-dev@users.noreply.github.com>
2021-06-02 16:18:25 -0400

    Update 117. Populating Next Right Pointers in Each Node II.java

--- Binary Tree/117. Populating Next Right Pointers in Each Node II/117. Populating Next Right Pointers in Each Node II.java
+++ Binary Tree/117. Populating Next Right Pointers in Each Node II/117. Populating Next Right Pointers in Each Node II.java
@@ -1,3 +1,5 @@
+//BFS
+//O(N)
 public Node connect(Node root) {
         if (root==null) return null;
         Queue<Node> q = new LinkedList<>();
@@ -15,4 +17,30 @@ public Node connect(Node root) {
             }            
         }
         return dummy;
+    }
+
+//constant space
+//O(1)
+public Node connect(Node root) {
+        if (root==null) return null;
+        Node cur = root;
+        while (cur!=null){
+            Node itr = cur;
+            Node dummy = new Node(0);
+            Node prev = dummy;
+            while (itr!=null){
+                if (itr.left!=null){
+                    prev.next = itr.left;
+                    prev = prev.next;
+                }
+                if (itr.right!=null){
+                    prev.next = itr.right;
+                    prev = prev.next;
+                }
+                itr = itr.next;
+            }
+            cur = dummy.next;
+            dummy.next = null;
+        }
+        return root;
     }
