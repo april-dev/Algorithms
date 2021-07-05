ad28e40d50ff3de783b05382e6faaab8d4abf0e1
april-dev <66917271+april-dev@users.noreply.github.com>
2021-07-04 23:29:57 -0400

    Update 188. Best Time to Buy and Sell Stock IV.java

--- DP/188. Best Time to Buy and Sell Stock IV/188. Best Time to Buy and Sell Stock IV.java
+++ DP/188. Best Time to Buy and Sell Stock IV/188. Best Time to Buy and Sell Stock IV.java
@@ -1,3 +1,29 @@
+/*
+dp[i][j] = maximum profit from at most i transactions using prices[0..j]
+
+A transaction is defined as one buy + sell.
+
+Now on day j, we have two options
+
+Do nothing (or buy) which doesn't change the acquired profit : dp[i][j] = dp[i][j-1]
+
+Sell the stock: In order to sell the stock, you must've bought it on a day t=[0..j-1]. Maximum profit that can be attained is t:0->j-1 max(prices[j]-prices[t]+dp[i-1][t-1]) 
+where prices[j]-prices[t] is the profit from buying on day t and selling on day j. dp[i-1][t-1] is the maximum profit that can be made with at most i-1 transactions with prices prices[0..t-1].
+
+Time complexity of this approach is O(n2k).
+
+In order to reduce it to O(nk), we must find t:0->j-1 max(prices[j]-prices[t]+dp[i-1][t-1]) this expression in constant time. If you see carefully,
+
+t:0->j-1 max(prices[j]-prices[t]+dp[i-1][t-1]) is same as
+
+prices[j] + t:0->j-1 max(dp[i-1][t-1]-prices[t])
+
+Second part of the above expression maxTemp = t:0->j-1 max(dp[i-1][t-1]-prices[t]) can be included in the dp loop by keeping track of the maximum value till j-1.
+
+*/
+
+
+
 //O(N^2*k) solution
 class Solution {
     public int maxProfit(int k, int[] prices) {
@@ -18,6 +44,20 @@ class Solution {
     }
 }
 
+//O(nk) solution
+public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        if (n==0) return 0;
+        int[][] dp = new int[k+1][n];
+        for (int i=1; i<=k; i++){
+            int tempMax = -prices[0];
+            for (int j=1; j<n; j++){
+                dp[i][j] = Math.max(dp[i][j-1], prices[j]+tempMax);
+                tempMax = Math.max(tempMax, dp[i-1][j-1]-prices[j]);
+            }
+        }
+        return dp[k][n-1];          
+    }
 
 
 
