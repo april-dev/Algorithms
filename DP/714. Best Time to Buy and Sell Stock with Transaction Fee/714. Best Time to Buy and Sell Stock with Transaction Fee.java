3de1c9ae01ad984306c7120e94fc4f6516b32140
april-dev <66917271+april-dev@users.noreply.github.com>
2021-07-06 22:14:43 -0400

    Update 714. Best Time to Buy and Sell Stock with Transaction Fee.java

--- DP/714. Best Time to Buy and Sell Stock with Transaction Fee/714. Best Time to Buy and Sell Stock with Transaction Fee.java
+++ DP/714. Best Time to Buy and Sell Stock with Transaction Fee/714. Best Time to Buy and Sell Stock with Transaction Fee.java
@@ -1,3 +1,24 @@
+public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int[] buy = new int[n];
+        int[] sell = new int[n];
+        buy[0] = -prices[0];
+        for (int i=1; i<n; i++){
+            buy[i] = Math.max(buy[i-1], sell[i-1]-prices[i]);
+            sell[i] = Math.max(sell[i-1], buy[i-1]+prices[i]-fee);
+        }
+        return sell[n-1];
+    }
+
+
+
+
+
+
+
+
+
+
 //See highest voted discussion
 
 public int maxProfit(int[] prices, int fee) {
