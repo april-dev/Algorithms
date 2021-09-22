34763eb6ab9d87e72fe00e97e6f56cd5a375fb95
april-dev <66917271+april-dev@users.noreply.github.com>
2021-09-22 11:12:19 -0400

    Update 1209. Remove All Adjacent Duplicates in String II.java

--- Stack/1209. Remove All Adjacent Duplicates in String II/1209. Remove All Adjacent Duplicates in String II.java
+++ Stack/1209. Remove All Adjacent Duplicates in String II/1209. Remove All Adjacent Duplicates in String II.java
@@ -30,3 +30,62 @@ class Node{
         this.count=count;
     }
 }
+
+//cleaned version
+Stack<Node> stack = new Stack<>();
+        for (int i=0; i<s.length(); i++){
+            if (stack.isEmpty() || stack.peek().c!=s.charAt(i)){
+                stack.add(new Node(s.charAt(i)));
+            }else{           
+                stack.peek().count++;
+                if (stack.peek().count==k) stack.pop();
+            
+            }           
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Node n: stack){
+            for (int i=0; i<n.count; i++){
+                sb.append(n.c);
+            }
+        }
+        return sb.toString();
+    }
+    class Node {
+        Character c;
+        int count;
+        public Node(char c){
+            this.c = c;
+            count = 1;
+        }
+
+
+
+//My solution
+public String removeDuplicates(String s, int k) {
+        Stack<Node> stack = new Stack<>();
+        for (int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            Node cur = new Node(c);
+            while (!stack.isEmpty() && stack.peek().c==c){
+                cur.count = stack.peek().count +1;
+                stack.pop();                                              
+            }            
+            if (cur.count<k) stack.add(cur);           
+        }            
+        StringBuilder sb = new StringBuilder();
+        for (Node n:stack){
+            for (int i=0; i<n.count; i++){
+                sb.append(n.c);
+            }
+        }
+        return sb.toString();       
+    }
+    
+    class Node{
+        char c;
+        int count;
+        public Node(char c){
+            this.c = c;
+            count = 1;
+        }
+    }
